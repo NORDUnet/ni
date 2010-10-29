4eb62e6baa620fecd5d82c2ab19343c0377a6688
Johan Lundberg <lundberg@nordu.net>
2010-10-29 16:27:46 +0200

    Added detail url.

--- scr/niweb/noclook/urls.py
+++ scr/niweb/noclook/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('niweb.noclook.views',
     (r'^$', 'index'),
-    (r'^(?P<handle_id>\d+)/$', 'detail'),
+    (r'^(?P<slug>[-\w]+)/(?P<handle_id>\d+)/$', 'detail'),
 )
