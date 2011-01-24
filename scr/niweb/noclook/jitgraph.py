a33fdb00f5928647166c5f8a75270630c35c459f
Johan Lundberg <lundberg@nordu.net>
2011-01-24 16:56:13 +0100

    neo4jclient.py: get_relationships() now dont care in which direction the relationships go. jitgraph.py: Working (but slow) traversal function done.

--- scr/niweb/noclook/jitgraph.py
+++ scr/niweb/noclook/jitgraph.py
@@ -2,6 +2,7 @@ import json
 import neo4jclient
 
 # Color and shape settings for know node types
+# http://thejit.org/static/v20/Docs/files/Options/Options-Node-js.html
 # Physical
 PHYSICAL = {'$type': 'circle',
         '$color': '#00FF00',
@@ -30,9 +31,11 @@ def get_jit_node(node):
     structure = {'id': node.id,
                 'name': '%s %s' % (node['type'], node['name']),
                 'data':{
+                    # JIT data
                     '$type': 'triangle',
                     '$color': '#EE3B3B', # Red
                     '$dim': '15',
+                    # Other data
                     'node_type': node['type']
                 },
                 'adjacencies':[]
@@ -73,6 +76,33 @@ def get_directed_adjacencie(rel):
 
     return structure
 
+def traverse_relationships(root_node, ttype, rtype):
+    '''
+    Traverse the relationship we want and add the nodes and
+    adjacencies to the JSON structure.
+    ttype is traversal type:    nc.Undirected.*
+                                nc.Outgoing.*
+                                nc.Incoming.*
+    rtype is the relation type as a string.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    graph_list = []
+    traverse_list = root_node.traverse(types=[ttype])
+    traverse_list.append(root_node)
+    # Needs to be done differently, to slooow.
+    for node in traverse_list:
+        for node2 in traverse_list:
+            for rel in nc.get_relationships(node, node2, rtype):
+                jit_node = get_jit_node(node)
+                jit_node['adjacencies'].append(
+                                        get_directed_adjacencie(rel))
+                jit_node2 = get_jit_node(node2)
+                jit_node2['adjacencies'].append(
+                                        get_directed_adjacencie(rel))
+                graph_list.append(jit_node)
+                graph_list.append(jit_node2)
+    return graph_list
+
 def create_graph_list(root_node):
     '''
     Creates a data structure from the root node and adjacent nodes.
@@ -88,32 +118,108 @@ def create_graph_list(root_node):
         'data':{}}
     ]}]
     '''
-
     nc = neo4jclient.Neo4jClient()
-    meta_type = nc.get_node_meta_type(node)
+    # Create graph lists for known node types
+    meta_type = nc.get_node_meta_type(root_node)
     if meta_type == 'physical':
-
+        graph_list = create_physical_graph_list(root_node)
     elif meta_type == 'logical':
-
+        graph_list = create_logical_graph_list(root_node)
     elif meta_type == 'relation':
-
+        graph_list = create_relation_graph_list(root_node)
     elif meta_type == 'location':
-
+        graph_list = create_location_graph_list(root_node)
     else:
         graph_list = []
         jit_root = get_jit_node(root_node)
         for rel in root_node.relationships.all():
             if rel.start.id != root_node.id:
-                node = get_jit_node(rel.start)
+                jit_node = get_jit_node(rel.start)
             else:
-                node = get_jit_node(rel.end)
+                jit_node = get_jit_node(rel.end)
             jit_root['adjacencies'].append(get_directed_adjacencie(rel))
-            node['adjacencies'].append(get_directed_adjacencie(rel))
+            jit_node['adjacencies'].append(get_directed_adjacencie(rel))
             graph_list.append(node)
         graph_list.append(jit_root)
 
-    # Remove all None items from the list
-    #graph_list = filter(lambda x: x is not None, graph_list)
+    return graph_list
+
+def create_physical_graph_list(root_node):
+    '''
+    Creates a graph_list that is specialized for visualizing a physical
+    node and its adjacencies.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    graph_list = []
+    graph_list.extend(traverse_relationships(root_node,
+                                                nc.Undirected.Has,
+                                                'Has'))
+    graph_list.extend(traverse_relationships(root_node,
+                                            nc.Undirected.Connected_to,
+                                            'Connected_to'))
+    graph_list.extend(traverse_relationships(root_node,
+                                            nc.Undirected.Depends_on,
+                                            'Depends_on'))
+    graph_list.extend(traverse_relationships(root_node,
+                                        nc.Undirected.Responsible_for,
+                                        'Responsible_for'))
+    graph_list.extend(traverse_relationships(root_node,
+                                        nc.Undirected.Located_in,
+                                        'Located_in'))
+    return graph_list
+
+def create_logical_graph_list(root_node):
+    '''
+    Creates a graph_list that is specialized for visualizing a physical
+    node and its adjacencies.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    graph_list = []
+    graph_list.extend(traverse_relationships(root_node,
+                                                nc.Undirected.Provides,
+                                                'Provides'))
+    graph_list.extend(traverse_relationships(root_node,
+                                            nc.Undirected.Uses,
+                                            'Uses'))
+    graph_list.extend(traverse_relationships(root_node,
+                                            nc.Undirected.Depends_on,
+                                            'Depends_on'))
+    return graph_list
+
+def create_relation_graph_list(root_node):
+    '''
+    Creates a graph_list that is specialized for visualizing a physical
+    node and its adjacencies.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    graph_list = []
+    graph_list.extend(traverse_relationships(root_node,
+                                                nc.Undirected.Provides,
+                                                'Provides'))
+    graph_list.extend(traverse_relationships(root_node,
+                                            nc.Undirected.Uses,
+                                            'Uses'))
+    graph_list.extend(traverse_relationships(root_node,
+                                        nc.Undirected.Responsible_for,
+                                        'Responsible_for'))
+    return graph_list
+
+def create_location_graph_list(root_node):
+    '''
+    Creates a graph_list that is specialized for visualizing a physical
+    node and its adjacencies.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    graph_list = []
+    graph_list.extend(traverse_relationships(root_node,
+                                                nc.Undirected.Has,
+                                                'Has'))
+    graph_list.extend(traverse_relationships(root_node,
+                                        nc.Undirected.Responsible_for,
+                                        'Responsible_for'))
+    graph_list.extend(traverse_relationships(root_node,
+                                        nc.Undirected.Located_in,
+                                        'Located_in'))
     return graph_list
 
 def get_json(root_node):
