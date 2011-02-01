ee955285b0919137e571e6c8faaecef3ed5420d9
Johan Lundberg <lundberg@nordu.net>
2011-02-01 18:51:24 +0100

    Changed the project tree to reflect the agreed upon layout.

--- scr/niweb/generic_settings.py
+++ scr/niweb/generic_settings.py
@@ -20,7 +20,7 @@ path.append(NIWEB_ROOT)
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = join(NIWEB_ROOT, 'media/')
+MEDIA_ROOT = join(NIWEB_ROOT, 'static/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -30,7 +30,7 @@ MEDIA_URL = NIWEB_MEDIA_URL
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = join(NIWEB_URL, 'media/admin/')
+ADMIN_MEDIA_PREFIX = join(NIWEB_URL, 'static/admin/')
 
 # URL of the login page.
 LOGIN_URL = '/login/'
