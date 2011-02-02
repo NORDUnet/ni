2a83a0e1446d6414e8725fc7f4503a069464cc7b
Johan Lundberg <lundberg@nordu.net>
2011-02-02 18:02:38 +0100

    Updated the layout of the project.

--- scr/niweb/generic_settings.py
+++ scr/niweb/generic_settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.flatpages',
     'django.contrib.comments',
     'django.contrib.markup',
-    'noclook',
+    'niweb_core',
+    'apps.noclook',
     'debug_toolbar',
 )
