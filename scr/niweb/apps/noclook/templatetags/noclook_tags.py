69abc5b98c9bf556576cfe7ccc725d6a26d65947
Johan Lundberg <lundberg@nordu.net>
2011-02-03 12:04:03 +0100

    Missed adding noclooks templatetags.

---
+++ scr/niweb/apps/noclook/templatetags/noclook_tags.py
@@ -0,0 +1,13 @@
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('type_menu.html')
+def type_menu():
+    '''
+    Returns a list with all wanted NodeType objects for easy menu
+    handling.
+    Just chain .exclude(type='name') to remove unwanted types.
+    '''
+    from niweb.apps.noclook.models import NodeType
+    types = NodeType.objects.exclude(type='PIC')
+    return {'types': types}
