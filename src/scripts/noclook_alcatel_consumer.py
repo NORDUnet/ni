bbf36c754a293844c3b4cae3e68cc8d0bab10c50
Johan Lundberg <lundberg@nordu.net>
2011-04-26 18:57:02 +0200

    Split up noclook_consumer.

---
+++ src/scripts/noclook_alcatel_consumer.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       noclook_alcatel_consumer.py
+#
+#       Copyright 2010 Johan Lundberg <lundberg@nordu.net>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+import os
+import sys
+import argparse
+
+## Need to change this path depending on where the Django project is
+## located.
+#path = '/var/norduni/src/niweb/'
+path = '/home/lundberg/norduni/src/niweb/'
+##
+##
+sys.path.append(os.path.abspath(path))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import neo4jclient
+import noclook_consumer as nt
+
+'''
+This script is used for adding the objects collected with the
+NERDS producers alcatel_isis to the NOCLook database viewer.
+
+JSON format used:
+[{"host": {
+    "alcatel_isis": {
+        "data": {
+            "ip_address": "", 
+            "link": "",
+            "name": "",
+            "osi_address": "", 
+            "ots": "", 
+            "type": ""
+        }, 
+        "name": "", 
+        "neighbours": [
+            {
+                "metric": "", 
+                "name": ""
+            }, 
+        ]
+    }, 
+    "name": "", 
+    "version": 1
+    }
+}]
+'''
+
+def insert_cable(cable_id, cable_type):
+    '''
+    Creates a new cable node and node_handle.
+    Returns the node in a node_list.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    node_handle = nt.get_unique_node_handle(cable_id, 'Cable', 'physical')
+    node = nc.get_node_by_id(node_handle.node_id)
+    node['cable_type'] = cable_type
+    return node_handle
+
+def consume_alcatel_isis(json_list):
+    '''
+    Inserts the data loaded from the json files created by the nerds
+    producer alcatel_isis.
+    
+    Metric = 0      "localhost"
+    Metric = 12     "LAN connected"
+    Metric = 19     "OMS - optical multiplex section connection"
+                    (between LM's only)
+    Metric = 20/21  "direct fiber connection"
+    '''
+    nc = neo4jclient.Neo4jClient()
+
+    # Insert the optical node
+    for i in json_list:
+        name = i['host']['alcatel_isis']['name']
+        node_handle = nt.get_unique_node_handle(name, 'Optical Node', 'physical')
+        node = nc.get_node_by_id(node_handle.node_id)
+        
+        data = i['host']['alcatel_isis']['data']
+        for key,value in data.items():
+            if value:
+                node[key] = value
+
+        for neighbour in i['host']['alcatel_isis']['neighbours']:
+            metric = neighbour['metric']
+            if metric == '0':       # localhost
+                break
+            elif metric == '12':    # LAN connected
+                cable_type = 'TP'
+            else:                   # Fiber
+                cable_type = 'Fiber'
+            # Get or create a neighbour node
+            neighbour_node_handle = nt.get_unique_node_handle(neighbour['name'],
+                                            'Optical Node', 'physical')
+            neighbour_node = nc.get_node_by_id(neighbour_node_handle.node_id)
+            # See if the nodes already are connected via something
+            create = True
+            for rel in node.relationships.incoming(['Connected_to']):
+                for rel2 in rel.start.relationships.outgoing(['Connected_to']):
+                    if rel2.end['name'] == neighbour_node['name']:
+                        create = False
+                        break
+            if create:
+                tmp_name = '%s - %s' % (node['name'], neighbour_node['name']) # Is this good until we get the fiber id?
+                cable_handle = insert_cable(tmp_name, cable_type)
+                cable_node = nc.get_node_by_id(cable_handle.node_id)
+                if not nc.get_relationships(cable_node, node, 'Connected_to'):
+                    # Only create a relationship if it doesn't exist
+                    cable_node.Connected_to(node)
+                if not nc.get_relationships(cable_node, neighbour_node, 
+                                                                'Connected_to'):
+                    # Only create a relationship if it doesn't exist
+                    cable_node.Connected_to(neighbour_node)
+
+def main():
+    # User friendly usage output
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-C', nargs='?',
+        help='Path to the configuration file.')
+    args = parser.parse_args()
+    
+    # Load the configuration file
+    if args.C == None:
+        print 'Please provide a configuration file with -C.'
+        sys.exit(1)
+    else:
+        config = nt.init_config(args.C)
+        alcatel_isis_data = config.get('data', 'alcatel_isis')
+        
+    if alcatel_isis_data:
+        consume_alcatel_isis(nt.load_json(alcatel_isis_data))
+        print 'noclook_alcatel_isis consume done.'
+
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ src/scripts/noclook_juniper_consumer.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       noclook_juniper_consumer.py
+#
+#       Copyright 2011 Johan Lundberg <lundberg@nordu.net>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+import os
+import sys
+import json
+import datetime
+import argparse
+import ipaddr
+
+## Need to change this path depending on where the Django project is
+## located.
+#path = '/var/norduni/src/niweb/'
+path = '/home/lundberg/norduni/src/niweb/'
+##
+##
+sys.path.append(os.path.abspath(path))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+import neo4jclient
+import noclook_consumer as nt
+
+'''
+This script is used for adding the objects collected with the
+NERDS producers juniper_config to the NOCLook database viewer.
+
+JSON format used:
+{["host": {
+    "juniper_conf": {
+        "bgp_peerings": [
+            {    
+            "as_number": "", 
+            "group": "", 
+            "description": "", 
+            "remote_address": "", 
+            "local_address": "", 
+            "type": ""
+            },
+        ], 
+        "interfaces": [
+            {
+            "name": "", 
+            "bundle": "", 
+            "vlantagging": true/false, 
+            "units": [
+                {
+                "address": [
+                "", 
+                ""
+                ], 
+                "description": "", 
+                "unit": "", 
+                "vlanid": ""
+                }
+            ], 
+            "tunnels": [
+            {
+            "source": "", 
+            "destination": ""
+            }
+            ], 
+            "description": ""
+            }, 
+        ],
+        "name": ""
+        }, 
+        "version": 1, 
+        "name": ""        
+    }
+]}
+'''
+
+def insert_juniper_router(name):
+    '''
+    Inserts a physical meta type node of the type Router.
+    Returns the node created.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    node_handle = nt.get_unique_node_handle(name, 'Router', 'physical')
+    node = nc.get_node_by_id(node_handle.node_id)
+    node_list = [node]
+
+    return node_list
+
+def insert_juniper_interfaces(router_node, interfaces):
+    '''
+    Insert all interfaces in the interfaces list with a Has
+    relationship from the router_node. Some filtering is done for
+    interface names that are not interesting.
+    Returns a list with all created nodes.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    not_interesting_interfaces = ['all', 'fxp0', '']
+    node_list = []
+    for i in interfaces:
+        name = i['name']
+        if name not in not_interesting_interfaces:
+            # Also "not interesting" is interfaces with . or * in their
+            # names
+            if '.' not in name and '*' not in name:
+                node_handle = nt.get_node_handle(name, 'PIC', 'physical', 
+                                                                    router_node)
+                node = nc.get_node_by_id(node_handle.node_id)
+                node['description'] = nt.rest_comp(i['description'])
+                node['units'] = json.dumps(i['units'])
+                if not nc.get_relationships(router_node, node, 'Has'):
+                    # Only create a relationship if it doesn't exist
+                    router_node.Has(node)
+                node_list.append(node)
+
+    return node_list
+
+def insert_juniper_relation(name, as_number):
+    '''
+    Inserts a relation meta type node of the type Peering partner.
+    Returns the newly created node.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    node_handle = nt.get_unique_node_handle(name, 'Peering Partner', 'relation')
+    node = nc.get_node_by_id(node_handle.node_id)
+    node['as_number'] = nt.rest_comp(as_number)
+    node_list = [node]
+
+    return node_list
+
+def insert_juniper_service(name):
+    '''
+    Inserts a logical meta type node of the type IP Service.
+    Returns the newly created node.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    node_handle = nt.get_unique_node_handle(name, 'IP Service', 'logical')
+    node = nc.get_node_by_id(node_handle.node_id)
+    node_list = [node]
+
+    return node_list
+
+def insert_juniper_bgp_peerings(bgp_peerings):
+    '''
+    Inserts all BGP peerings for all routers collected by the
+    juniper_conf producer. This is to be able to get all the internal
+    peerings associated to the right interfaces.
+    Returns a list of all created peering nodes.
+    '''
+    nc = neo4jclient.Neo4jClient()
+    for p in bgp_peerings:
+        name = p['description']
+        if name == None:
+            name = 'No description'
+
+        group = p['group']
+        service = nc.get_node_by_value(group, 'logical', 'name')
+        if not service:
+            service = insert_juniper_service(group)
+
+        peering_type = p['type']
+        if peering_type == 'internal':
+            remote_addr = ipaddr.IPAddress(p['remote_address'])
+            local_addr = ipaddr.IPAddress(p['local_address'])
+
+        elif peering_type == 'external':
+            peeringp = nc.get_node_by_value(p['as_number'], 'relation', 'as_number')
+            if not peeringp:
+                peeringp = insert_juniper_relation(name, p['as_number'])
+            rel_uses = nc.get_relationships(peeringp[0], service[0], 'Uses')
+            create = True
+            for rel in rel_uses:
+                if rel['ip_address'] == p['remote_address']:
+                    # Already have this relationship
+                    create = False
+            if create:
+                # Only create a relationship if it doesn't exist
+                peeringp[0].Uses(service[0], ip_address=p['remote_address'])
+            remote_addr = ipaddr.IPAddress(p['remote_address'])
+            local_addr = ipaddr.IPAddress('0.0.0.0') #None did not work
+
+        # Loop through interfaces to find the local and/or remote
+        # address
+        for pic in nc.get_node_by_value('PIC', 'physical', 'type'):            
+            units = json.loads(pic['units'])
+            # Gah, this next part needs to be refactored, it is hard
+            # to read and ugly...
+            for unit in units:     
+                for addr in unit['address']:
+                    try:
+                        pic_addr = ipaddr.IPNetwork(addr)
+                    except ValueError:
+                        # ISO address on lo0
+                        break
+                    if local_addr in pic_addr or \
+                                            remote_addr in pic_addr:
+                        rels = nc.get_relationships(service[0],
+                                            pic, 'Depends_on')
+                        create = True # Create new relation
+                        for rel in rels:
+                            # Can't have more than one unit with the
+                            # same unit number
+                            if rel['unit'] == unit['unit']:
+                                create = False  # Do not create a
+                                break           # new relation
+                        if create:
+                            service[0].Depends_on(pic,
+                                    ip_address=addr,
+                                    unit=unit['unit'],
+                                    vlan=unit['vlanid'],
+                                    description=unit['description'])
+                            break
+
+def consume_juniper_conf(json_list):
+    '''
+    Inserts the data loaded from the json files created by the nerds
+    producer juniper_conf.
+    Some filtering is done for interface names that are not interesting.
+    '''
+    bgp_peerings = []
+    for i in json_list:
+        name = i['host']['juniper_conf']['name']
+        router_node = insert_juniper_router(name)[0]
+        interfaces = i['host']['juniper_conf']['interfaces']
+        insert_juniper_interfaces(router_node,
+                            interfaces)
+        bgp_peerings += i['host']['juniper_conf']['bgp_peerings']
+        
+    insert_juniper_bgp_peerings(bgp_peerings)
+
+def main():
+    # User friendly usage output
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-C', nargs='?',
+        help='Path to the configuration file.')
+    parser.add_argument('-I', action='store_true',
+        help='Insert data in to the database.')
+    args = parser.parse_args()
+    
+    # Start time
+    start = datetime.datetime.now()
+    timestamp_start = datetime.datetime.strftime(start,
+        '%b %d %H:%M:%S')
+    print '%s noclook_juniper_consumer.py was started.' % timestamp_start
+
+    # Load the configuration file
+    if args.C == None:
+        print 'Please provide a configuration file with -C.'
+        sys.exit(1)
+    else:
+        config = nt.init_config(args.C)
+
+    # Insert data from known data sources if option -I was used
+    if args.I:
+        print 'Inserting data...'
+        if config.get('data', 'juniper_conf'):
+            consume_juniper_conf(nt.load_json(
+                                    config.get('data', 'juniper_conf')))
+    # end time
+    end = datetime.datetime.now()
+    timestamp_end = datetime.datetime.strftime(end,
+        '%b %d %H:%M:%S')
+    print '%s noclook_juniper_consumer.py ran successfully.' % timestamp_end
+    timedelta = end - start
+    print 'Total time: %s' % (timedelta)
+    return 0
+
+if __name__ == '__main__':
+    main()
