6f4ea896640736e40d30ace12c45ef1dd65b6747
Johan Lundberg <lundberg@nordu.net>
2011-04-28 16:30:54 +0200

    Added get_user function to get or create a user on demand.

--- src/scripts/noclook_consumer.py
+++ src/scripts/noclook_consumer.py
@@ -60,6 +60,27 @@ def init_config(path):
     except IOError as (errno, strerror):
         print "I/O error({0}): {1}".format(errno, strerror)
 
+def rest_comp(data):
+    '''
+    As the REST interface cant handle None type change None to False.
+    '''
+    if data is None:
+        return False
+    else:
+        return data
+        
+def load_json(json_dir):
+    '''
+    Thinks all files in the supplied dir are text files containing json.
+    '''
+    json_list = []
+
+    for subdir, dirs, files in os.walk(json_dir):
+        for file in files:
+            f=open(join(json_dir, file), 'r')
+            json_list.append(json.load(f))
+    return json_list
+
 def test_db():
     handles = NodeHandle.objects.all()
     nc = neo4jclient.Neo4jClient()
@@ -77,6 +98,21 @@ def purge_db():
             print 'Could not delete the Neo4j node.' 
     NodeHandle.objects.all().delete()
 
+def get_user(username='noclook'):
+    '''
+    Gets or creates a user that can be used to insert data.
+    '''
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        # Password generation taken from
+        # http://code.activestate.com/recipes/576722-pseudo-random-string/
+        import os, math
+        from base64 import b64encode
+        passwd = b64encode(os.urandom(int(math.ceil(0.75*30))),'-_')[:30]
+        user = User.objects.create_user(username, '', passwd)
+    return user
+    
 def get_node_type(type_name):
     '''
     Returns or creates and returns the NodeType object with the supplied
@@ -89,7 +125,6 @@ def get_node_type(type_name):
         from django.template.defaultfilters import slugify
         node_type = NodeType(type=type_name, slug=slugify(type_name))
         node_type.save()
-
     return node_type
 
 def get_unique_node_handle(node_name, node_type_name, node_meta_type):
@@ -100,7 +135,7 @@ def get_unique_node_handle(node_name, node_type_name, node_meta_type):
     Returns a NodeHandle object.
     '''
     # Hard coded user value that we can't get on the fly right now
-    user = User.objects.get(username='lundberg')
+    user = get_user()
     node_type = get_node_type(node_type_name)
     try:
         node_handle = NodeHandle.objects.get(node_name=node_name,
@@ -122,7 +157,7 @@ def get_node_handle(node_name, node_type_name, node_meta_type,
     Returns a NodeHandle object.
     '''
     # Hard coded user value that we can't get on the fly right now
-    user = User.objects.get(username='lundberg')
+    user = get_user()
     node_type = get_node_type(node_type_name)
     try:
         node_handles = NodeHandle.objects.filter(
@@ -145,275 +180,6 @@ def get_node_handle(node_name, node_type_name, node_meta_type,
                             creator=user)
     node_handle.save()
     return node_handle # No NodeHandle found return a new handle.
-
-def rest_comp(data):
-    '''
-    As the REST interface cant handle None type we change None to False.
-    '''
-    if data is None:
-        return False
-    else:
-        return data
-        
-def load_json(json_dir):
-    '''
-    Thinks all files in the supplied dir are text files containing json.
-    '''
-    json_list = []
-
-    for subdir, dirs, files in os.walk(json_dir):
-        for file in files:
-            f=open(join(json_dir, file), 'r')
-            json_list.append(json.load(f))
-
-    return json_list
-
-#def insert_juniper_router(name):
-#    '''
-#    Inserts a physical meta type node of the type Router.
-#    Returns the node created.
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#    node_handle = get_unique_node_handle(name, 'Router', 'physical')
-#    node = nc.get_node_by_id(node_handle.node_id)
-#    node_list = [node]
-#
-#    return node_list
-#
-#def insert_juniper_interfaces(router_node, interfaces):
-#    '''
-#    Insert all interfaces in the interfaces list with a Has
-#    relationship from the router_node. Some filtering is done for
-#    interface names that are not interesting.
-#    Returns a list with all created nodes.
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#    not_interesting_interfaces = ['all', 'fxp0', '']
-#    node_list = []
-#    for i in interfaces:
-#        name = i['name']
-#        if name not in not_interesting_interfaces:
-#            # Also "not interesting" is interfaces with . or * in their
-#            # names
-#            if '.' not in name and '*' not in name:
-#                node_handle = get_node_handle(name, 'PIC', 'physical', router_node)
-#                node = nc.get_node_by_id(node_handle.node_id)
-#                node['description'] = rest_comp(i['description'])
-#                node['units'] = json.dumps(i['units'])
-#                if not nc.get_relationships(router_node, node, 'Has'):
-#                    # Only create a relationship if it doesn't exist
-#                    router_node.Has(node)
-#                node_list.append(node)
-#
-#    return node_list
-#
-#def insert_juniper_relation(name, as_number):
-#    '''
-#    Inserts a relation meta type node of the type Peering partner.
-#    Returns the newly created node.
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#    node_handle = get_unique_node_handle(name, 'Peering Partner', 'relation')
-#    node = nc.get_node_by_id(node_handle.node_id)
-#    node['as_number'] = rest_comp(as_number)
-#    node_list = [node]
-#
-#    return node_list
-#
-#def insert_juniper_service(name):
-#    '''
-#    Inserts a logical meta type node of the type IP Service.
-#    Returns the newly created node.
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#    node_handle = get_unique_node_handle(name, 'IP Service', 'logical')
-#    node = nc.get_node_by_id(node_handle.node_id)
-#    node_list = [node]
-#
-#    return node_list
-#
-#def insert_juniper_bgp_peerings(bgp_peerings):
-#    '''
-#    Inserts all BGP peerings for all routers collected by the
-#    juniper_conf producer. This is to be able to get all the internal
-#    peerings associated to the right interfaces.
-#    Returns a list of all created peering nodes.
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#    for p in bgp_peerings:
-#        name = p['description']
-#        if name == None:
-#            name = 'No description'
-#
-#        group = p['group']
-#        service = nc.get_node_by_value(group, 'logical', 'name')
-#        if not service:
-#            service = insert_juniper_service(group)
-#
-#        peering_type = p['type']
-#        if peering_type == 'internal':
-#            remote_addr = ipaddr.IPAddress(p['remote_address'])
-#            local_addr = ipaddr.IPAddress(p['local_address'])
-#
-#        elif peering_type == 'external':
-#            peeringp = nc.get_node_by_value(p['as_number'], 'relation', 'as_number')
-#            if not peeringp:
-#                peeringp = insert_juniper_relation(name, p['as_number'])
-#            rel_uses = nc.get_relationships(peeringp[0], service[0], 'Uses')
-#            create = True
-#            for rel in rel_uses:
-#                if rel['ip_address'] == p['remote_address']:
-#                    # Already have this relationship
-#                    create = False
-#            if create:
-#                # Only create a relationship if it doesn't exist
-#                peeringp[0].Uses(service[0], ip_address=p['remote_address'])
-#            remote_addr = ipaddr.IPAddress(p['remote_address'])
-#            local_addr = ipaddr.IPAddress('0.0.0.0') #None did not work
-#
-#        # Loop through interfaces to find the local and/or remote
-#        # address
-#        for pic in nc.get_node_by_value('PIC', 'physical', 'type'):          
-#            units = json.loads(pic['units'])
-#            # Gah, this next part needs to be refactored, it is hard
-#            # to read and ugly...
-#            for unit in units:
-#                for addr in unit['address']:
-#                    try:
-#                        pic_addr = ipaddr.IPNetwork(addr)
-#                    except ValueError:
-#                        # ISO address on lo0
-#                        break
-#                    if local_addr in pic_addr or \
-#                                            remote_addr in pic_addr:
-#                        rels = nc.get_relationships(service[0],
-#                                            pic, 'Depends_on')
-#                        create = True # Create new relation
-#                        for rel in rels:
-#                            # Can't have more than one unit with the
-#                            # same unit number
-#                            if rel['unit'] == unit['unit']:
-#                                create = False  # Do not create a
-#                                break           # new relation
-#                        if create:
-#                            service[0].Depends_on(pic,
-#                                    ip_address=addr,
-#                                    unit=unit['unit'],
-#                                    vlan=unit['vlanid'],
-#                                    description=unit['description'])
-#                            break
-#
-#def consume_juniper_conf(json_list):
-#    '''
-#    Inserts the data loaded from the json files created by the nerds
-#    producer juniper_conf.
-#    Some filtering is done for interface names that are not interesting.
-#    '''
-#    bgp_peerings = []
-#    for i in json_list:
-#        name = i['host']['juniper_conf']['name']
-#        router_node = insert_juniper_router(name)[0]
-#        interfaces = i['host']['juniper_conf']['interfaces']
-#        insert_juniper_interfaces(router_node,
-#                            interfaces)
-#        bgp_peerings += i['host']['juniper_conf']['bgp_peerings']
-#
-#    insert_juniper_bgp_peerings(bgp_peerings)
-
-
-#def insert_nmap(json_list):
-#    '''
-#    Inserts the data loaded from the json files created by
-#    the nerds producer nmap_services.
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#
-#    # Hard coded values that we can't get on the fly right now
-#    #user = User.objects.get(username="lundberg")
-#    node_type = "Host"
-#    meta_type = 'physical'
-#
-#    # Insert the host
-#    for i in json_list:
-#        name = i['host']['name']
-#        hostnames = json.dumps(i['host']['hostnames'])
-#        addresses = json.dumps(i['host']['addrs'])
-#        try:
-#            services = json.dumps(i['host']['services'])
-#        except KeyError:
-#            services = 'None'
-#
-#        # Create the NodeHandle and the Node
-#        node_handle = get_unique_node_handle(name, node_type, meta_type)
-#        #node_handle = NodeHandle(node_name=name, node_type=type,
-#            #node_meta_type = meta_type, creator=user)
-#        #node_handle.save()
-#
-#        # Set Node attributes
-#        node = nc.get_node_by_id(node_handle.node_id)
-#        node['hostnames'] = hostnames
-#        node['addresses'] = addresses
-#        node['services'] = services
-
-#def insert_cable(cable_id, cable_type):
-#    '''
-#    Creates a new cable node and node_handle.
-#    Returns the node in a node_list.
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#    node_handle = get_unique_node_handle(cable_id, 'Cable', 'physical')
-#    node = nc.get_node_by_id(node_handle.node_id)
-#    node['cable_type'] = cable_type
-#    return node_handle
-#
-#def consume_alcatel_isis(json_list):
-#    '''
-#    Inserts the data loaded from the json files created by the nerds
-#    producer alcatel_isis.
-#
-#    Metric = 0      "localhost"
-#    Metric = 12     "LAN connected"
-#    Metric = 19     "OMS - optical multiplex section connection"
-#                    (between LM's only)
-#    Metric = 20/21  "direct fiber connection"
-#    '''
-#    nc = neo4jclient.Neo4jClient()
-#
-#    # Insert the optical node
-#    for i in json_list:
-#        name = i['host']['alcatel_isis']['name']
-#        node_handle = get_unique_node_handle(name, 'Optical Node', 'physical')
-#        node = nc.get_node_by_id(node_handle.node_id)
-#        for neighbour in i['host']['alcatel_isis']['neighbours']:
-#            metric = neighbour['metric']
-#            if metric == '0':       # localhost
-#                break
-#            elif metric == '12':    # LAN connected
-#                cable_type = 'TP'
-#            else:                   # Fiber
-#                cable_type = 'Fiber'
-#            # Get or create a neighbour node
-#            neighbour_node_handle = get_unique_node_handle(neighbour['name'],
-#                                            'Optical Node', 'physical')
-#            neighbour_node = nc.get_node_by_id(neighbour_node_handle.node_id)
-#            # See if the nodes already are connected via something
-#            create = True
-#            for rel in node.relationships.incoming(['Connected_to']):
-#                for rel2 in rel.start.relationships.outgoing(['Connected_to']):
-#                    if rel2.end['name'] == neighbour_node['name']:
-#                        create = False
-#                        break
-#            if create:
-#                tmp_name = '%s - %s' % (node['name'], neighbour_node['name']) # Is this good until we get the fiber id?
-#                cable_handle = insert_cable(tmp_name, cable_type)
-#                cable_node = nc.get_node_by_id(cable_handle.node_id)
-#                if not nc.get_relationships(cable_node, node, 'Connected_to'):
-#                    # Only create a relationship if it doesn't exist
-#                    cable_node.Connected_to(node)
-#                if not nc.get_relationships(cable_node, neighbour_node, 
-#                                                                'Connected_to'):
-#                    # Only create a relationship if it doesn't exist
-#                    cable_node.Connected_to(neighbour_node)
 
 def consume_noclook(json_list):
     '''
@@ -453,9 +219,6 @@ def consume_noclook(json_list):
             
             for key,value in properties.items():
                 rel[key] = value
-                
-#    for n in nc.get_all_nodes():
-#        if n.
 
 def main():
     # User friendly usage output
@@ -514,7 +277,8 @@ def main():
             noclook_alcatel_consumer.consume_alcatel_isis(data)
             print 'alcatel_isis consume done.'
         if noclook_data:
-            consume_noclook(load_json(config.get('data', 'noclook')))
+            data = load_json(noclook_data)
+            consume_noclook(data)
             print 'noclook consume done.'
 
     # end time
