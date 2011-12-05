4aeb92887162003aa75c09845324531570b7fee4
Johan Lundberg <lundberg@nordu.net>
2011-12-05 17:08:28 +0100

    Rewrote edit views.

--- src/niweb/apps/noclook/views.py
+++ src/niweb/apps/noclook/views.py
@@ -452,8 +452,9 @@ def new_node(request, slug):
         node_handle = NodeHandle(node_name=node_name,
                                 node_type=node_type,
                                 node_meta_type=node_meta_type,
-                                creator=request.user)
+                                modifier=request.user, creator=request.user)
         node_handle.save()
+        nc.set_noclook_auto_manage(nc.neo4jdb, node_handle.get_node(), False)
         return edit_node(request, slugify(node_handle.node_type), 
                                                          node_handle.handle_id)
     else:
@@ -475,28 +476,29 @@ def edit_node(request, slug, handle_id, node=None, message=None):
     if not node:
         node = nh.get_node()
     # Make a dict of properties you want to be able to change
-    node_properties = {}    
-    for key, value in node.properties.items():
-        try:
-            node_properties[key] = json.dumps(value)
-        except ValueError:
-            node_properties[key] = value
-    unwanted_properties = ['handle_id', 'node_type']
-    for key in unwanted_properties:
-        del node_properties[key]
-    
+    node_properties = {}
+    unwanted_properties = ['handle_id', 'node_type', 'noclook_auto_manage',
+                           'noclook_last_seen']
+    for key, value in node.items():
+        if key not in unwanted_properties:
+            try:
+                node_properties[key] = json.dumps(value)
+            except ValueError:
+                node_properties[key] = value
     # Relationships
     # Make a dict of relationships you want to be able to change
     unwanted_relationships = ['Contains', 'Consist_of']
     node_relationships = []
-    for rel in node.relationships.all():
+    for rel in node.relationships:
         relationship = {'properties': {}}
-        if rel.type not in unwanted_relationships:
+        if rel.type.name() not in unwanted_relationships:
             relationship['id'] = rel.id
             relationship['start'] = rel.start['name']
-            relationship['type'] = rel.type
+            relationship['type'] = rel.type.name()
             relationship['end'] = rel.end['name']
-            relationship['properties'].update(rel.properties)
+            for key in rel.getPropertyKeys():
+                if key not in unwanted_properties:
+                    relationship[key] = rel[key]
             node_relationships.append(relationship)
     return render_to_response('noclook/edit_node.html',
                             {'node_handle': nh, 'node': node,
@@ -534,9 +536,11 @@ def save_node(request, slug, handle_id):
         for item in post:
             new_properties[item] = post.get(item)
         # Update the node
-        node = nc.update_node_properties(nh.node_id, new_properties)
+        node = nc.update_item_properties(nc.neo4jdb, node, new_properties)
         # Update the node_handle
         nh.node_name = node['name']
+        nh.modifier = request.user
+        nc.update_noclook_auto_manage(nc.neo4jdb, node)
         nh.save()
     return edit_node(request, slug, handle_id, node=node)
     
@@ -573,24 +577,26 @@ def new_relationship(request, slug, handle_id):
         if request.POST['direction']:
             direction = request.POST['direction']
             node_id = request.POST['nodes']
-            other_node = nc.get_node_by_id(node_id)
+            other_node = nc.get_node_by_id(nc.neo4jdb, node_id)
             rel_type = request.POST['types']
             if direction == 'out':
-                rel = nc.make_suitable_relationship(node, other_node, rel_type)
+                rel = nc.create_suitable_relationship(nc.neo4jdb, node,
+                                                      other_node, rel_type)
             else:
-                rel = nc.make_suitable_relationship(other_node, node, rel_type)
+                rel = nc.create_suitable_relationship(nc.neo4jdb, other_node,
+                                                      node, rel_type)
             if rel:
-                rel_id = rel.id
-                return edit_relationship(request, slug, handle_id, rel_id, rel)
+                nc.set_noclook_auto_manage(nc.neo4jdb, rel, False)
+                return edit_relationship(request, slug, handle_id, rel.id, rel)
             else:
                 message = 'The requested relationship could not be made.' 
         else:
             message = 'You have to choose relationship direction.'
     node_dicts = []
-    suitable_nodes = nc.get_suitable_nodes(node)
+    suitable_nodes = nc.get_suitable_nodes(nc.neo4jdb, node)
     for item in ['physical', 'logical', 'relation', 'location']:
         for n in suitable_nodes[item]:
-            parent = nc.get_root_parent(n, nc.Incoming.Has)
+            parent = nc.get_root_parent(nc.neo4jdb, n)
             if parent:
                 name = '%s %s' % (parent['name'], n['name'])      
             else:
@@ -613,9 +619,10 @@ def edit_relationship(request, slug, handle_id, rel_id, rel=None, message=None):
         raise Http404
     nh = get_object_or_404(NodeHandle, pk=handle_id)
     if not rel:
-        node = nh.get_node()
-        rel = nc.get_relationship_by_id(rel_id, node)
-    rel_properties = rel.properties
+        rel = nc.get_relationship_by_id(nc.neo4jdb, rel_id)
+    rel_properties = {}
+    for key in rel.getPropertyKeys():
+        rel_properties[key] = rel[key]
     return render_to_response('noclook/edit_relationship.html',
                             {'node_handle': nh, 'rel': rel, 
                              'rel_properties': rel_properties, 
@@ -626,9 +633,7 @@ def edit_relationship(request, slug, handle_id, rel_id, rel=None, message=None):
 def save_relationship(request, slug, handle_id, rel_id):
     if not request.user.is_staff:
         raise Http404
-    nh = get_object_or_404(NodeHandle, pk=handle_id)
-    node = nh.get_node()
-    rel = nc.get_relationship_by_id(rel_id, node)
+    rel = nc.get_relationship_by_id(nc.neo4jdb, rel_id)
     if request.POST:
         # request.POST is immutable.
         post = request.POST.copy()
@@ -649,7 +654,8 @@ def save_relationship(request, slug, handle_id, rel_id):
         for item in post:
             new_properties[item] = post.get(item)
         # Update the relationships properties
-        rel = nc.update_relationship_properties(node.id, rel_id, new_properties)
+        rel = nc.update_item_properties(nc.neo4jdb, rel, new_properties)
+        nc.update_noclook_auto_manage(nc.neo4jdb, rel)
     return edit_relationship(request, slug, handle_id, rel_id, rel)
 
 @login_required
@@ -663,13 +669,14 @@ def delete_relationship(request, slug, handle_id, rel_id):
         nh = get_object_or_404(NodeHandle, pk=handle_id)
         node = nh.get_node()
         message = 'No relationship matching the query was found. Nothing deleted.'
-        for rel in node.relationships.all():
+        for rel in node.relationships:
             cur_id = str(rel.id)
             if cur_id == rel_id and cur_id in request.POST['confirmed']:
                 message = 'Relationship %s %s %s deleted.' % (rel.start['name'],
                                                               rel.type,
                                                               rel.end['name']) 
-                rel.delete()
+                with nc.neo4jdb.transaction:
+                    rel.delete()
                 break                
         return edit_node(request, slug, handle_id, message=message)
     else:            
