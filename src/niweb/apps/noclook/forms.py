e1fbe2415dd51d7273ac65ecd83d82a8a65af7be
Johan Berggren <jbn@nordu.net>
2011-12-13 13:43:30 +0100

    Form design

--- src/niweb/apps/noclook/forms.py
+++ src/niweb/apps/noclook/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 
-COUNTRIES = [('SE', 'Sweden'), ('DK', 'Denmark')]
+COUNTRIES = [
+    ('SE', 'Sweden'),
+    ('DK', 'Denmark'),
+    ('FI', 'Finland'),
+    ('NO', 'Norway'),
+    ('DE', 'Germany'),
+    ('IS', 'Iceland'),
+    ('UK', 'Great Britain'),
+    ('US', 'United States'),
+    ('NL', 'Netherlands')
+]
 
 class SiteForm(forms.Form):
     name = forms.CharField()
-    country = forms.ChoiceField(choices = COUNTRIES, widget = forms.widgets.RadioSelect())
+    country_code = forms.ChoiceField(choices=COUNTRIES, widget=forms.widgets.RadioSelect)
