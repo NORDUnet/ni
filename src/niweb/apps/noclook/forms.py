aa0b9325b5210b511e82ea4e38e0b8eb0e916814
Johan Lundberg <lundberg@nordu.net>
2011-12-15 11:25:20 +0100

    First edit_site template and view.

--- src/niweb/apps/noclook/forms.py
+++ src/niweb/apps/noclook/forms.py
@@ -12,9 +12,34 @@ COUNTRIES = [
     ('NL', 'NL')
 ]
 
+SITE_TYPES = [
+    ('Unknown', 'Unknown'),
+    ('POP', 'POP'),
+    ('Regenerator', 'Regenerator'),
+    ('Optical Amplifier', 'Optical Amplifier')
+]
+
 class NewSiteForm(forms.Form):
     name = forms.CharField()
+    country_code = forms.ChoiceField(choices=COUNTRIES,
+                                     widget=forms.widgets.Select)
     address = forms.CharField(required=False)
     postarea = forms.CharField(required=False)
     postcode = forms.CharField(required=False)
-    country_code = forms.ChoiceField(choices=COUNTRIES, widget=forms.widgets.Select)
+    
+    
+class EditSiteForm(forms.Form):
+    name = forms.CharField()
+    country_code = forms.ChoiceField(choices=COUNTRIES,
+                                     widget=forms.widgets.Select)
+    site_type = forms.ChoiceField(choices=SITE_TYPES,
+                                  widget=forms.widgets.RadioSelect, required=False)
+    address = forms.CharField(required=False)
+    postarea = forms.CharField(required=False)
+    postcode = forms.CharField(required=False)
+    longitude = forms.DecimalField(required=False)
+    latitude = forms.DecimalField(required=False)
+    telenor_subscription_id = forms.CharField(required=False)
+    owner_id = forms.CharField(required=False)
+    
+    
