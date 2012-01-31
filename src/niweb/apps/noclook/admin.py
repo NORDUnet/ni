24c11d834904d05edb27e5d7044bcf0021d24bbb
Johan Lundberg <lundberg@nordu.net>
2012-01-31 11:34:42 +0100

    You can no filter on node_type and creator for node_handles in the admin interface.

--- src/niweb/apps/noclook/admin.py
+++ src/niweb/apps/noclook/admin.py
@@ -2,12 +2,10 @@ from apps.noclook.models import NodeHandle, NodeType
 from django.contrib import admin
 
 class NodeHandleAdmin(admin.ModelAdmin):
-    {
-    'fields': ('node_name', 'node_type', 'creator')
-    }
+    list_filter = ('node_type', 'creator')
 
 class NodeTypeAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('type',)}
 
-admin.site.register(NodeHandle)
+admin.site.register(NodeHandle, NodeHandleAdmin)
 admin.site.register(NodeType, NodeTypeAdmin)
