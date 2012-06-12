e4dcb0d19182cbbd52d3c8eb81f9c81dd5e94aa1
Johan Lundberg <lundberg@nordu.net>
2012-06-12 18:32:35 +0200

    Added Host Reports with Host Users reports.

---
+++ src/niweb/apps/noclook/report_views.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2012-06-11 5:48 PM
+
+@author: lundberg
+"""
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from niweb.apps.noclook.forms import get_node_type_tuples
+import norduni_client as nc
+
+@login_required
+def host_reports(request):
+    return render_to_response('noclook/reports/host_reports.html',{},
+                              context_instance=RequestContext(request))
+
+@login_required
+def host_users(request, host_user_name=None):
+    host_users = dict([[name,id] for id,name in get_node_type_tuples('Host User') if name])
+    host_user_id = host_users.get(host_user_name, None)
+    header = ['host_user', 'host_name', 'last_seen']
+    if host_user_id:
+        num_of_hosts_q = '''
+            START node=node({id})
+            MATCH node-[r:Uses|Owns]->host
+            RETURN COUNT(host) as num_of_hosts
+            '''
+        num_of_hosts_hits = nc.neo4jdb.query(num_of_hosts_q, id=host_user_id)
+        num_of_hosts = [hit['num_of_hosts'] for hit in num_of_hosts_hits][0]
+        hosts_q = '''
+            START node=node({id})
+            MATCH node-[r:Uses|Owns]->host
+            RETURN node.name as host_user, host, host.name as host_name, host.noclook_last_seen as last_seen
+            '''
+        hosts = nc.neo4jdb.query(hosts_q, id=host_user_id)
+    elif host_user_name == 'Missing':
+        num_of_hosts_q = '''
+                START host=node:node_types(node_type = "Host")
+                MATCH host<-[r?:Uses|Owns]-()
+                WHERE r is null
+                RETURN COUNT(host) as num_of_hosts
+                '''
+        num_of_hosts_hits = nc.neo4jdb.query(num_of_hosts_q, id=host_user_id)
+        num_of_hosts = [hit['num_of_hosts'] for hit in num_of_hosts_hits][0]
+        hosts_q = '''
+                START host=node:node_types(node_type = "Host")
+                MATCH host<-[r?:Uses|Owns]-()
+                WHERE r is null
+                RETURN host, host.name as host_name, host.noclook_last_seen as last_seen
+                '''
+        hosts = nc.neo4jdb.query(hosts_q, id=host_user_id)
+    else:
+        host_user_name = 'All'
+        num_of_hosts_q = '''
+                START node=node:node_types(node_type = "Host User")
+                MATCH node-[r:Uses|Owns]->host
+                RETURN COUNT(node) as num_of_hosts
+                '''
+        num_of_hosts_hits = nc.neo4jdb.query(num_of_hosts_q, id=host_user_id)
+        num_of_hosts = [hit['num_of_hosts'] for hit in num_of_hosts_hits][0]
+        hosts_q = '''
+                START node=node:node_types(node_type = "Host User")
+                MATCH node-[r:Uses|Owns]->host
+                RETURN node.name as host_user, host, host.name as host_name, host.noclook_last_seen as last_seen
+                '''
+        hosts = nc.neo4jdb.query(hosts_q, id=host_user_id)
+    return render_to_response('noclook/reports/host-users.html',
+            {'host_user_name': host_user_name, 'host_users': host_users,
+            'num_of_hosts': num_of_hosts, 'header': header, 'hosts': hosts},
+            context_instance=RequestContext(request))
--- src/niweb/apps/noclook/templatetags/noclook_tags.py
+++ src/niweb/apps/noclook/templatetags/noclook_tags.py
@@ -1,21 +1,34 @@
 from niweb.apps.noclook.models import NodeType
 from niweb.apps.noclook.helpers import get_node_url
+from datetime import datetime
 from django import template
 register = template.Library()
 
 @register.inclusion_tag('type_menu.html')
 def type_menu():
-    '''
+    """
     Returns a list with all wanted NodeType objects for easy menu
     handling.
     Just chain .exclude(type='name') to remove unwanted types.
-    '''
+    """
     types = NodeType.objects.exclude(type='PIC').exclude(type='Unit').exclude(type='Port')
     return {'types': types}
 
 @register.simple_tag
 def noclook_node_to_url(node):
-    '''
+    """
     Takes a node id as a string and returns the absolute url for a node.
-    '''
+    """
     return get_node_url(node)
+
+@register.assignment_tag
+def noclook_last_seen_to_dt(noclook_last_seen):
+    """
+    Returns noclook_last_seen property (ex. 2011-11-01T14:37:13.713434) as a
+    datetime.datetime. If a datetime cant be made None is returned.
+    """
+    try:
+        dt = datetime.strptime(noclook_last_seen, '%Y-%m-%dT%H:%M:%S.%f')
+    except ValueError:
+        dt = None
+    return dt
