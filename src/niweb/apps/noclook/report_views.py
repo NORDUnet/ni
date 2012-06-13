f48f4936cda88d72695502a6f0ac75df602414ed
Johan Lundberg <lundberg@nordu.net>
2012-06-13 10:46:51 +0200

    The host user report can now be downloaded in csv or excel format.

--- src/niweb/apps/noclook/report_views.py
+++ src/niweb/apps/noclook/report_views.py
@@ -10,6 +10,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from niweb.apps.noclook.forms import get_node_type_tuples
+from niweb.apps.noclook.helpers import nodes_to_csv, nodes_to_xls
 import norduni_client as nc
 
 @login_required
@@ -18,10 +19,11 @@ def host_reports(request):
                               context_instance=RequestContext(request))
 
 @login_required
-def host_users(request, host_user_name=None):
+def host_users(request, host_user_name=None, form=None):
     host_users = dict([[name,id] for id,name in get_node_type_tuples('Host User') if name])
+    hosts = []
+    num_of_hosts = 0
     host_user_id = host_users.get(host_user_name, None)
-    header = ['host_user', 'host_name', 'last_seen']
     if host_user_id:
         num_of_hosts_q = '''
             START node=node({id})
@@ -52,8 +54,7 @@ def host_users(request, host_user_name=None):
                 RETURN host, host.name as host_name, host.noclook_last_seen as last_seen
                 '''
         hosts = nc.neo4jdb.query(hosts_q, id=host_user_id)
-    else:
-        host_user_name = 'All'
+    elif host_user_name == 'All':
         num_of_hosts_q = '''
                 START node=node:node_types(node_type = "Host User")
                 MATCH node-[r:Uses|Owns]->host
@@ -67,7 +68,13 @@ def host_users(request, host_user_name=None):
                 RETURN node.name as host_user, host, host.name as host_name, host.noclook_last_seen as last_seen
                 '''
         hosts = nc.neo4jdb.query(hosts_q, id=host_user_id)
+    if form:
+        header = ['host_user', 'host_name', 'last_seen']
+        if form == 'csv':
+            return nodes_to_csv([host for host in hosts], header=header)
+        elif form == 'xls':
+            return nodes_to_xls([host for host in hosts], header=header)
     return render_to_response('noclook/reports/host-users.html',
             {'host_user_name': host_user_name, 'host_users': host_users,
-            'num_of_hosts': num_of_hosts, 'header': header, 'hosts': hosts},
+            'num_of_hosts': num_of_hosts, 'hosts': hosts},
             context_instance=RequestContext(request))
