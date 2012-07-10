897a3dd5751f8c645163f7e7c830c1c3dff22d17
Johan Lundberg <lundberg@nordu.net>
2012-07-10 11:57:14 +0200

    Added two database fix scripts for updating the operational database.

---
+++ src/scripts/neo4j/fix_auto_manage.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2012-07-10 11:07 AM
+
+@author: lundberg
+"""
---
+++ src/scripts/neo4j/fix_cable_type.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2012-07-10 11:41 AM
+
+@author: lundberg
+"""
