2ded360d4c5dda788f871fc1f228100f6c33d663
Johan Lundberg <lundberg@nordu.net>
2013-06-19 14:18:00 +0200

    Set verbose to False as standard.

--- src/scripts/noclook_checkmk_consumer.py
+++ src/scripts/noclook_checkmk_consumer.py
@@ -43,7 +43,7 @@ from apps.noclook import helpers as h
 # This script is used for adding the objects collected with the
 # NERDS producer checkmk_livestatus.
 
-VERBOSE = True
+VERBOSE = False
 
 """
 {
