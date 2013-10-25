a961db11e98f819077490d17c358ecef3e7a5fa2
Johan Lundberg <lundberg@nordu.net>
2013-10-25 11:08:05 +0200

    Added a REST client and a script that uses the RESTlike api to get data from NI.

---
+++ src/scripts/rest/apiclient.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2013-10-24 1:57 PM
+
+@author: lundberg
+"""
+
+import hammock
+import json
+from itertools import count
+import logging
+
+logger = logging.getLogger('niapiclient')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+class NIApiClient():
+
+    def __init__(self, base_url, user, apikey):
+        self.user = user
+        self.apikey = apikey
+        self.client = hammock.Hammock(base_url, append_slash=True)
+
+    def create_headers(self, **kwargs):
+        headers = {'Authorization': 'ApiKey %s:%s' % (self.user, self.apikey)}
+        headers.update(kwargs)
+        return headers
+
+    def get_type(self, node_type, limit=20, headers={}):
+        for offset in count(start=0, step=limit):
+            request = self.client.api.v1(node_type).GET(headers=headers, params={'limit': limit, 'offset': offset})
+            try:
+                batch = json.loads(request.content)['objects']
+            except ValueError as e:
+                logger.error(e)
+                logger.error('%s %s' % (request.status_code, request.reason))
+                break
+            if not batch:
+                raise StopIteration
+            for obj in batch:
+                yield obj
+
+    def get_relationships(self, entity, limit=20, relationship_type=None, headers={}):
+        try:
+            pk = entity['handle_id']
+            node_type = entity['node_type'].split('/')[-2]
+        except KeyError as e:
+            logger.error(e)
+            logger.error('entity did not supply expected values.')
+            raise KeyError
+        for offset in count(start=0, step=limit):
+            params = {'limit': limit, 'offset': offset}
+            if not relationship_type:
+                request = self.client.api.v1(node_type)(pk).relationships.GET(headers=headers, params=params)
+            else:
+                request = self.client.api.v1(node_type)(pk).relationships(relationship_type).GET(headers=headers,
+                                                                                                 params=params)
+            try:
+                batch = json.loads(request.content)['objects']
+            except ValueError as e:
+                logger.error(e)
+                logger.error('%s %s' % (request.status_code, request.reason))
+                break
+            if not batch:
+                raise StopIteration
+            for obj in batch:
+                yield obj
--- src/scripts/rest/hosts_with_ports.py
+++ src/scripts/rest/hosts_with_ports.py
@@ -1,12 +1,53 @@
 # -*- coding: utf-8 -*-
 __author__ = 'lundberg'
 
-import hammock
-import json
+import sys
+import argparse
+from collections import defaultdict
+from apiclient import NIApiClient
 
-base_url = 'https://nidev-consumer.nordu.net'
-user = 'lundberg'
-key = '204db7bcfafb2deb7506b89eb3b9b715b09905c8'
-nidev = hammock.Hammock(base_url, append_slash=True)
-headers = {'Authorization': 'ApiKey %s:%s' % (user, key)}
-hosts = json.loads(nidev.api.v1.host.GET(headers=headers).content)['objects']
+USER = 'lundberg'
+APIKEY = '204db7bcfafb2deb7506b89eb3b9b715b09905c8'
+BASE_URL = 'http://localhost'
+VERBOSE = False
+
+
+def get_hosts(output_file):
+    client = NIApiClient(BASE_URL, USER, APIKEY)
+    for host in client.get_type('host', headers=client.create_headers()):
+        if host['node'].get('operational_state', 'Not set') != 'Decommissioned':
+            if VERBOSE:
+                print 'Getting ports for %s...' % host['node_name'],
+            ports = defaultdict(list)
+            for rel in client.get_relationships(host, relationship_type='Depends_on', headers=client.create_headers()):
+                protocol = rel['properties'].get('protocol', None)
+                port = rel['properties'].get('port', None)
+                if protocol and port:
+                    ports[protocol].append(port)
+            tcp_ports, udp_ports = '', ''
+            if 'tcp' in ports:
+                tcp_ports = 'T:%s,' % ','.join([str(i) for i in set(ports['tcp'])])
+            if 'udp' in ports:
+                udp_ports = 'U:%s' % ','.join([str(i) for i in set(ports['udp'])])
+            if tcp_ports or udp_ports:
+                for ip_address in host['node'].get('ip_addresses', []):
+                    output_file.writelines('%s %s%s\n' % (ip_address, tcp_ports, udp_ports))
+            if VERBOSE:
+                print 'done.'
+
+
+def main():
+    # User friendly usage output
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', '-O', default=sys.stdout, type=argparse.FileType('wb', 0))
+    parser.add_argument('--verbose', '-V', action='store_true', default=False)
+    args = parser.parse_args()
+    if args.verbose:
+        global VERBOSE
+        VERBOSE = True
+    get_hosts(args.output)
+    args.output.close()
+    return 0
+
+if __name__ == '__main__':
+    main()
