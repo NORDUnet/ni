3d19fc5c907175838f58b3de30aac69ea2cdf6c0
Johan Lundberg <lundberg@nordu.net>
2014-04-03 17:38:16 +0200

    Added ProviderResource to the api.

--- src/niweb/apps/noclook/api/resources.py
+++ src/niweb/apps/noclook/api/resources.py
@@ -536,6 +536,19 @@ class PeeringPartnerResource(NodeHandleResource):
         filtering = {
             "node_name": ALL,
         }
+
+
+class ProviderResource(NodeHandleResource):
+
+    class Meta:
+        queryset = NodeHandle.objects.filter(node_type__slug__exact='provider')
+        resource_name = 'provider'
+        authentication = ApiKeyAuthentication()
+        authorization = Authorization()
+        allowed_methods = ['get', 'put', 'post']
+        filtering = {
+            "node_name": ALL,
+        }
         
       
 class PortResource(NodeHandleResource):
