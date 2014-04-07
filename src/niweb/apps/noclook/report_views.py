44d0e1d6d32bbc8ea503764121341a7a3ba71618
Johan Lundberg <lundberg@nordu.net>
2014-04-07 14:46:50 +0200

    Updated mail subject and contract reference for netapp report as specified by admin.

--- src/niweb/apps/noclook/report_views.py
+++ src/niweb/apps/noclook/report_views.py
@@ -286,16 +286,15 @@ def quarterly_netapp_usage():
         service_node = nc.get_unique_node_by_name(nc.neo4jdb, service['service_id'], 'Service')
         try:
             customers = ', '.join([hit['customer'].get_property('name', '') for hit in h.get_customer(service_node)])
-        except Exception:
+        except Exception:  # Neo4j embedded jpype exception
             customers = 'Missing customer name'
-        contract_number = service_node.get_property('contract_number', 'Missing contract number')
         data_dict = json.loads(service_node.get_property('netapp_storage_monthly', '{}')).get(year, None)
         if data_dict:
             for month in quarter_month_map[last_quarter]:
                 key = '%s-%s' % (year, month)
                 report_data.append({key: data_dict.get(month.lstrip('0'), 0.0)})
             # Create and send the mail
-            subject = 'NOCLook NetApp storage report for %s' % service['service_id']
+            subject = 'Storage cost for %s Q%d %s' % (service['contract_reference'], last_quarter, year)
             heading = 'Adobe connect storage volume billing Q%d, %s, for %s.' % (last_quarter, year, customers)
             body = '''
         This is an auto generated report from NOCLook for service ID %s.
@@ -305,12 +304,12 @@ def quarterly_netapp_usage():
         This report was generated on %s UTC.
             ''' % (service['service_id'], heading, utcnow.strftime('%Y-%m-%d %H:%M'))
             extended_to = to + extra_report.get(service['service_id'], [])  # Avoid changing REPORTS_TO :)
-            filename = 'NetApp storage report for %s Q%d %s.xls' % (service['service_id'], last_quarter, year)
+            filename = 'Storage cost for %s Q%d %s.xls' % (service['contract_reference'], last_quarter, year)
             wb = h.dicts_to_xls({}, [], '%s Q%d %s' % (service['service_id'], last_quarter, year))
             # Calculate and write pricing info
             ws = wb.get_sheet(0)
             ws.write(1, 1, heading)
-            ws.write(2, 1, 'Contract number: %s' % contract_number)
+            ws.write(2, 1, 'Reference: %s' % service['contract_reference'])
             ws.write(4, 1, 'Month')
             ws.write(4, 2, 'Storage (GB)')
             ws.write(4, 3, 'Service inc.')
