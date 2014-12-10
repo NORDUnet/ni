631a197c4d05a7fe4b1637fb7ff96202db3971c5
Johan Lundberg <lundberg@nordu.net>
2014-12-10 16:29:27 +0100

    Not a boolean.

--- src/niweb/apps/noclook/report_views.py
+++ src/niweb/apps/noclook/report_views.py
@@ -121,9 +121,9 @@ def host_services(request, status=None):
         else:
             where_statement = ''
             if status == 'locked':
-                where_statement = 'and node.services_locked! = true'
+                where_statement = 'and node.services_locked! = "True"'
             elif status == 'not-locked':
-                where_statement = 'and (not(has(node.services_locked)) or node.services_locked! = false)'
+                where_statement = 'and (not(has(node.services_locked)) or node.services_locked! = "False")'
             q = """
                 START node=node:node_types(node_type="Host")
                 WHERE not(node.operational_state! = "Decommissioned") %s
