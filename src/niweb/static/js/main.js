8184ef69780b679299b36d3a87a8d0cb9269375b
Markus Krogh <markus@nordu.net>
2015-01-22 10:24:45 +0000

    Generalizing the async load

---
+++ src/niweb/static/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  function async_load(url, trigger,target) {
+    var $trigger = $(trigger);
+    $trigger.one("click", function(){
+      $.get(url, function(data){
+        $(target).html(data);
+      }, "html");
+    });
+  }
+
+  $("[data-async-load]").each(function(idx){
+    var url = $(this).data("async-load");
+    var target = $(this).data("async-load-target");
+    async_load(url,this,target);
+  });
+});
