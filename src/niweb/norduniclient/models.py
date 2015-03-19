87c98095151e7319af8a177da405a424e65262ab
Johan Lundberg <lundberg@nordu.net>
2015-03-19 14:31:11 +0100

    Show dangling Cables in Equipment ports.

--- src/niweb/norduniclient/models.py
+++ src/niweb/norduniclient/models.py
@@ -486,7 +486,8 @@ class EquipmentModel(PhysicalModel):
     def get_connections(self):
         q = """
             MATCH (n:Node {handle_id: {handle_id}})-[:Has*1..10]->(porta)
-            OPTIONAL MATCH (n)-[:Has*0..10]->(porta)<-[r0:Connected_to]-(cable)-[r1:Connected_to]->(portb)
+            OPTIONAL MATCH (porta)<-[r0:Connected_to]-(cable)
+            OPTIONAL MATCH (porta)<-[r0:Connected_to]-(cable)-[r1:Connected_to]->(portb)
             OPTIONAL MATCH (portb)<-[:Has*1..10]-(end)
             OPTIONAL MATCH (end)-[:Located_in]->(location)
             OPTIONAL MATCH (location)<-[:Has]-site
