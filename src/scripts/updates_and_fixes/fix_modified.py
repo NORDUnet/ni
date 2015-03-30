16450f79ac6bdde57c6e92f5c65d47f9d5ceeb27
Johan Lundberg <lundberg@nordu.net>
2015-03-30 14:06:09 +0200

    Added python path configuration for fix script.

--- src/scripts/updates_and_fixes/fix_modified.py
+++ src/scripts/updates_and_fixes/fix_modified.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 __author__ = 'lundberg'
 
+import os
+import sys
+
+## Need to change this path depending on where the Django project is
+## located.
+base_path = '../../niweb/'
+sys.path.append(os.path.abspath(base_path))
+niweb_path = os.path.join(base_path, 'niweb')
+sys.path.append(os.path.abspath(niweb_path))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
 from apps.noclook.models import NodeHandle
 
 for nh in NodeHandle.objects.all():
