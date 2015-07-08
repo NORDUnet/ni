2e691c1d5db9d3ab79a5eb7a428ecda43f6448f7
Markus Krogh <markus@nordu.net>
2015-07-08 12:27:16 +0000

    Apply cleanup to pagination

--- src/niweb/apps/noclook/templatetags/url_tags.py
+++ src/niweb/apps/noclook/templatetags/url_tags.py
@@ -10,6 +10,7 @@ def paginate_path(context, value, param="page"):
     query = context['request'].GET.copy()
 
     query[param] = value
+    query = clean_queryparams(query)
     return query.urlencode()
   
 @register.simple_tag(takes_context=True)
@@ -18,9 +19,9 @@ def export_as(context, file_type, param_del=["page"]):
       return ""
     req = context['request']
     params = req.GET.copy()
-    for param,val in  req.GET.items():
-      if param in param_del or not val:
+    for param in  param_del:
         del params[param]
+    params = clean_queryparams(params)
     if req.path.endswith("/"):
         path = req.path[:-1]
     else:
@@ -29,4 +30,12 @@ def export_as(context, file_type, param_del=["page"]):
     if params:
         path +="?" + params.urlencode() 
     return path 
+
+def clean_queryparams(params):
+    out = params.copy()
+    for param,val in params.items():
+      if not val:
+        del out[param]
+    return out
+
 
