0c86c7ff5b09a85709e28bedce7ed8050c793ba2
Markus Krogh <markus@nordu.net>
2015-07-10 14:49:33 +0000

    Adding setting

--- src/niweb/niweb/generic_settings.py
+++ src/niweb/niweb/generic_settings.py
@@ -145,6 +145,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
+    'apps.noclook.announcements.page_flash',
 )
 
 ROOT_URLCONF = 'niweb.urls'
