b23cce35ad8fd857a63a2fb2bdaace7ea2ba392c
Johan Lundberg <lundberg@nordu.net>
2015-07-21 11:08:03 +0200

    Have a sane default settings env variable.

--- src/scripts/noclook_consumer.py
+++ src/scripts/noclook_consumer.py
@@ -35,7 +35,7 @@ base_path = '../niweb/'
 sys.path.append(os.path.abspath(base_path))
 niweb_path = os.path.join(base_path, 'niweb')
 sys.path.append(os.path.abspath(niweb_path))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "niweb.settings.prod")
 import django
 from django.conf import settings as django_settings
 from apps.noclook.models import NodeType, NodeHandle
