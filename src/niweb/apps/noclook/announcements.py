547efabadab9e0dc9d266451b4a995bde4c0735e
Johan Lundberg <lundberg@nordu.net>
2015-07-21 11:56:07 +0200

    Path variable change in settings.

--- src/niweb/apps/noclook/announcements.py
+++ src/niweb/apps/noclook/announcements.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 import os
 def page_flash(request):
-    path = os.path.join(settings.NIWEB_ROOT, "niweb", "page_message.txt")
+    path = os.path.join(settings.SITE_ROOT, "niweb", "page_message.txt")
     msg = None
     if os.path.isfile(path):
         with open(path, 'r') as f:
