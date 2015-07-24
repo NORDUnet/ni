7345098f718ebe47585c2af924a0bf34f6de1a6a
Johan Lundberg <lundberg@nordu.net>
2015-07-24 14:03:52 +0200

    Catching missing table exception in forms init that prevented migration.

--- src/niweb/apps/noclook/forms/__init__.py
+++ src/niweb/apps/noclook/forms/__init__.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
 __author__ = 'lundberg'
+
+import logging
+from django.db.utils import ProgrammingError
+from dynamic_preferences import global_preferences_registry
 from .common import *
-from .nordunet import *
-from .sunet import *
+
+logger = logging.getLogger('noclook.forms')
+
+try:
+    # Load forms for data domain specified in dynamic preferences
+    global_preferences = global_preferences_registry.manager()
+    domain = global_preferences.get('general__data_domain', '')
+
+    try:
+        if domain:
+            exec('from .{} import *'.format(domain))
+    except ImportError as e:
+        logger.error(e)
+        logger.error('Customized forms not loaded.')
+
+except ProgrammingError:
+    logger.critical('Could not find dynamic preferences table. Have you migrated the db?')
