02988b6c9064dd02c24de97beed3dafc13328555
Markus Krogh <markus@nordu.net>
2015-10-13 09:50:52 +0000

    Adding queue items

--- src/niweb/apps/scan/urls.py
+++ src/niweb/apps/scan/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
-    url(r'^host/(?P<hostname>[^/]+)/$', views.host, name='host'),
+    url(r'^queue/$', views.QueueIndexView.as_view(), name='queue'),
+    url(r'^host/$', views.host, name='host'),
 ]
