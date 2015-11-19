8f77a78c5da63a78244942dc7485fef42e829aac
Markus Krogh <markus@nordu.net>
2015-11-19 13:13:58 +0000

    Adding "Rescan"

--- src/niweb/apps/scan/urls.py
+++ src/niweb/apps/scan/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^queue/$', views.QueueIndexView.as_view(), name='queue'),
     url(r'^host/$', views.host, name='host'),
+    url(r'^queue/(?P<pk>\d+)/rescan$', views.rescan, name='rescan'),
 ]
