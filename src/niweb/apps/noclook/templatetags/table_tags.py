06237acf09fd15868c37d32e93c62955b5f84759
Markus Krogh <markus@nordu.net>
2016-06-24 14:34:00 +0200

    Hot fix for production type(item) is dict fails

--- src/niweb/apps/noclook/templatetags/table_tags.py
+++ src/niweb/apps/noclook/templatetags/table_tags.py
@@ -7,18 +7,19 @@ register = template.Library()
 def table_column(context,item):
     if not item:
         result = u''
-    elif type(item) is dict:
-        if "handle_id" in item:
-            # item is a node
-            result = noclook_node_to_link(context, item)
-        elif "url" in item:
-            # it is a 'link'
-            result = u'<a href="{}">{}</a>'.format(item.get('url',''),item.get('name',''))
-        else:
-            # Nothing to print
-            result = u''
+    # Hot fix for production since type(dict) does not work
+    elif type(item) is int:
+        result = item
     elif type(item) is list:
         result = u'<br> '.join([table_column(context, i) for i in item])
+    #TODO: type(item) is dict fails on production
+    #elif type(item) is dict:
+    elif "handle_id" in item:
+        # item is a node
+        result = noclook_node_to_link(context, item)
+    elif "url" in item:
+        # it is a 'link'
+        result = u'<a href="{}">{}</a>'.format(item.get('url',''),item.get('name',''))
     else:
         # Just print it
         result = item
