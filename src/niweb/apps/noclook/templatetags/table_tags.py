7ef9d7a467bd1c98fc311bcf94dd3561f5f720ae
Markus Krogh <markus@nordu.net>
2016-06-27 13:59:58 +0200

    From hot patch to more correct

--- src/niweb/apps/noclook/templatetags/table_tags.py
+++ src/niweb/apps/noclook/templatetags/table_tags.py
@@ -1,3 +1,4 @@
+import collections
 from django import template
 from apps.noclook.templatetags.noclook_tags import noclook_node_to_link
 
@@ -7,19 +8,17 @@ register = template.Library()
 def table_column(context,item):
     if not item:
         result = u''
-    # Hot fix for production since type(dict) does not work
-    elif type(item) is int:
-        result = item
     elif type(item) is list:
         result = u'<br> '.join([table_column(context, i) for i in item])
-    #TODO: type(item) is dict fails on production
-    #elif type(item) is dict:
-    elif "handle_id" in item:
-        # item is a node
-        result = noclook_node_to_link(context, item)
-    elif "url" in item:
-        # it is a 'link'
-        result = u'<a href="{}">{}</a>'.format(item.get('url',''),item.get('name',''))
+    elif isinstance(item, collections.Iterable):
+        if "handle_id" in item:
+            # item is a node
+            result = noclook_node_to_link(context, item)
+        elif "url" in item:
+            # it is a 'link'
+            result = u'<a href="{}">{}</a>'.format(item.get('url',''),item.get('name',''))
+        else:
+            result = u''
     else:
         # Just print it
         result = item
