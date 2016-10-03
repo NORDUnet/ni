6c549e330f9f8f8d175ef6e914cb56ea9cc115ee
Markus Krogh <markus@nordu.net>
2016-10-03 15:52:50 +0200

    ups default should be production

--- src/scripts/noclook_juniper_consumer.py
+++ src/scripts/noclook_juniper_consumer.py
@@ -35,7 +35,7 @@ base_path = '../niweb/'
 sys.path.append(os.path.abspath(base_path))
 niweb_path = os.path.join(base_path, 'niweb')
 sys.path.append(os.path.abspath(niweb_path))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "niweb.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "niweb.settings.prod")
 
 import noclook_consumer as nt
 from apps.noclook import helpers
