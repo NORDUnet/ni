c7cb53eacc9ad43e9e7c5eec06aa77811608b004
Markus Krogh <markus@nordu.net>
2017-03-31 13:34:08 +0000

    Fix str unicode problem with iterable

--- src/niweb/apps/noclook/templatetags/table_tags.py
+++ src/niweb/apps/noclook/templatetags/table_tags.py
@@ -4,22 +4,25 @@ from apps.noclook.templatetags.noclook_tags import noclook_node_to_link
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
-def table_column(context,item):
+def table_column(context, item):
     if not item:
         result = u''
     elif type(item) is list:
         result = u'<br> '.join([table_column(context, i) for i in item])
+    elif type(item) in (str, unicode):
+        result = item
     elif isinstance(item, collections.Iterable):
         if "handle_id" in item:
             # item is a node
             result = noclook_node_to_link(context, item)
         elif "url" in item:
             # it is a 'link'
-            result = u'<a href="{}">{}</a>'.format(item.get('url',''),item.get('name',''))
+            result = u'<a href="{}">{}</a>'.format(item.get('url', ''), item.get('name', ''))
         else:
-            # could be str
-            result =item
+            # fallback to default
+            result = item
     else:
         # Just print it
         result = item
