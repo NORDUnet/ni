2e7632c32d4a42b348c8c9641596a682a5b4ac31
Markus Krogh <markus@nordu.net>
2017-07-26 12:18:00 +0000

    Update last dependencies + fix == is in announce

--- src/niweb/apps/noclook/announcements.py
+++ src/niweb/apps/noclook/announcements.py
@@ -5,7 +5,7 @@ def page_flash(request):
     global_preferences = global_preferences_registry.manager()
     msg = global_preferences['announcements__page_flash_message']
     level = global_preferences['announcements__page_flash_message_level']
-    if msg == 'None' or msg == None or not msg.strip():
+    if msg == 'None' or msg is None or not msg.strip():
         msg = ''
     return {'page_flash': {'level': level, 'message': msg}}
 
