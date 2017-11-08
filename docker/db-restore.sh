de1132100b50a8d5848b855234a41c81949a82a5
Markus Krogh <markus@nordu.net>
2017-11-08 17:54:24 +0100

    Adding db-restore script

---
+++ docker/db-restore.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+set -e
+pushd `dirname $0` > /dev/null
+SCRIPT_DIR="$(pwd)"
+popd > /dev/null
+
+usage="Usage: $0 [-h] [-f <postgres.sql.gz>] -d <ni_store>"
+while getopts ":f:d:" opt; do
+  case $opt in
+    f) SQL_DUMP="$OPTARG";;
+    d) NI_STORE="$OPTARG";;
+    *) echo $usage
+       exit 1;;
+  esac
+done
+
+if [ ! -d "$NI_STORE" ]; then
+  echo "Error: NI_STORE is not defined, use -d <ni_store>"
+  exit 1
+fi
+
+if [ -z "$SQL_DUMP" ]; then
+  SQL_DUMP="$NI_STORE/producers/noclook/sql/postgres.sql.gz"
+fi
+
+if [ ! -f "$SQL_DUMP" ]; then
+  echo "Error: $SQL_DUMP does not exist, use -f <postgres.sql.gz>"
+  exit 1
+fi
+
+
+function now() {
+  date +"%Y-%m-%d %H:%M:%S"
+}
+
+function msg() {
+  echo "> $1 - $(now)"
+}
+
+function duration {
+  local T=$1
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  printf '%02d:%02d:%02d' $H $M $S
+}
+
+SECONDS=0
+
+set +e
+msg "Stopping neo4j"
+docker-compose -f $SCRIPT_DIR/compose-dev.yml stop neo4j
+
+msg "Removing neo4j data"
+rm -r $SCRIPT_DIR/data/neo4j/databases
+
+set -e
+
+msg "Starting neo4j again"
+docker-compose -f  $SCRIPT_DIR/compose-dev.yml start neo4j
+
+postgres_id=$(docker ps | awk '/postgres/ {print $1}')
+
+msg "Drop, Create DB"
+cat << EOM | docker exec -i $postgres_id psql -q postgres postgres
+DROP DATABASE norduni;
+CREATE DATABASE norduni;
+GRANT ALL PRIVILEGES ON DATABASE norduni to ni;
+ALTER USER ni CREATEDB;
+EOM
+
+msg "Import DB from $SQL_DUMP"
+gunzip -c $SQL_DUMP | docker exec -i $postgres_id psql -q -o /dev/null norduni ni
+
+msg "Django migrate"
+docker-compose -f docker/compose-dev.yml  run --rm norduni manage migrate
+
+
+msg "Import neo4j data from json"
+cat << EOM | docker-compose -f docker/compose-dev.yml  run --rm -v $NI_STORE/producers/noclook/json/:/opt/noclook norduni consume
+# Set after how many days data should be considered old.
+[data_age]
+juniper_conf = 30
+
+# Set if the consumer should check for old data and delete it.
+[delete_data]
+juniper_conf = false
+
+# All producers need to be listed here with a path to their data
+[data]
+juniper_conf =
+nmap_services_py =
+alcatel_isis =
+nagios_checkmk =
+cfengine_report =
+# noclook is used to import a already made backup
+noclook = /opt/noclook
+EOM
+
+msg "Reset postgres sequences"
+cat <<EOM | docker exec -i $postgres_id psql -q -o /dev/null norduni ni
+BEGIN;
+SELECT setval(pg_get_serial_sequence('"noclook_nodetype"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "noclook_nodetype";
+SELECT setval(pg_get_serial_sequence('"noclook_nodehandle"','handle_id'), coalesce(max("handle_id"), 1), max("handle_id") IS NOT null) FROM "noclook_nodehandle";
+SELECT setval(pg_get_serial_sequence('"noclook_uniqueidgenerator"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "noclook_uniqueidgenerator";
+SELECT setval(pg_get_serial_sequence('"noclook_nordunetuniqueid"','id'), coalesce(max("id"), 1), max("id") IS NOT null) FROM "noclook_nordunetuniqueid";
+COMMIT;
+EOM
+
+echo ""
+echo "> Finished db-restore in $(duration $SECONDS )"
+echo "" 
+
+msg "Create superuser"
+docker-compose -f docker/compose-dev.yml  run --rm norduni manage createsuperuser
+
