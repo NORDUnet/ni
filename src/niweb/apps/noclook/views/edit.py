cb1f1ae79fbfa30963b604f06e2b9eb40436d9b7
Markus Krogh <markus@nordu.net>
2019-04-04 13:55:26 +0200

    Add optical path

--- src/niweb/apps/noclook/views/edit.py
+++ src/niweb/apps/noclook/views/edit.py
@@ -561,7 +561,7 @@ def edit_optical_path(request, handle_id):
     nh, path = helpers.get_nh_node(handle_id)
     relations = path.get_relations()
     depends_on = path.get_dependencies()
-    dependency_categories = 'odf,optical-link,optical-multiplex-section,optical-node,router,switch'
+    dependency_categories = 'odf,optical-link,optical-multiplex-section,optical-node,router,switch,optical-path'
     if request.POST:
         form = forms.EditOpticalPathForm(request.POST)
         if form.is_valid():
