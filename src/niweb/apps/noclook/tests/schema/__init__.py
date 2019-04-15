29b9bb8324309df6380c72f4827b72b32b8af2bc
Francisco Fuentes <ffuentes@emergya.com>
2019-04-15 16:59:44 +0200

    Added first test

---
+++ src/niweb/apps/noclook/tests/schema/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ffuentes'
+
+from ..neo4j_base import NeoTestCase
+
+class Neo4jGraphQLTest(NeoTestCase):
+    def setUp(self):
+        super(Neo4jGraphQLTest, self).setUp()
+
+        # create nodes
+        organization1 = self.create_node('organization1', 'organization', meta='Logical')
+        organization2 = self.create_node('organization2', 'organization', meta='Logical')
+        contact1 = self.create_node('contact1', 'contact', meta='Relation')
+        contact2 = self.create_node('contact2', 'contact', meta='Relation')
+        role1 = self.create_node('role1', 'role', meta='Logical')
+        role2 = self.create_node('role2', 'role', meta='Logical')
+        group1 = self.create_node('group1', 'group', meta='Logical')
+        group2 = self.create_node('group2', 'group', meta='Logical')
+
+        # add some data
+        contact1_data = {
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+            'name': 'Jane Doe',
+        }
+
+        for key, value in contact1_data.items():
+            contact1.get_node().add_property(key, value)
+
+        contact2_data = {
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'name': 'John Smith',
+        }
+
+        for key, value in contact2_data.items():
+            contact2.get_node().add_property(key, value)
+
+        # create relationships
+        contact1.get_node().add_role(role1.handle_id)
+        contact1.get_node().add_group(group1.handle_id)
+        contact1.get_node().add_organization(organization1.handle_id)
+
+        contact2.get_node().add_role(role2.handle_id)
+        contact2.get_node().add_group(group2.handle_id)
+        contact2.get_node().add_organization(organization2.handle_id)
--- src/niweb/apps/noclook/tests/schema/test_schema.py
+++ src/niweb/apps/noclook/tests/schema/test_schema.py
@@ -1,2 +1,49 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ffuentes'
+
+from collections import OrderedDict
+from . import Neo4jGraphQLTest
+from niweb.schema import schema
+
+class QueryTest(Neo4jGraphQLTest):
+    def test_get_contacts(self):
+        query = '''
+        query getLastTenContacts {
+          contacts(limit: 10) {
+            handle_id
+            name
+            first_name
+            last_name
+            is_roles {
+              name
+            }
+            member_of_groups {
+              name
+            }
+          }
+        }
+        '''
+
+        expected = {
+            'contacts': [
+                OrderedDict([
+                    ('handle_id', '4'),
+                    ('name', 'John Smith'),
+                    ('first_name', 'John'),
+                    ('last_name', 'Smith'),
+                    ('is_roles', [OrderedDict([('name', 'role2')])]),
+                    ('member_of_groups', [OrderedDict([('name', 'group2')])]),
+                ]),
+                OrderedDict([
+                    ('handle_id', '3'),
+                    ('name', 'Jane Doe'),
+                    ('first_name', 'Jane'),
+                    ('last_name', 'Doe'),
+                    ('is_roles', [OrderedDict([('name', 'role1')])]),
+                    ('member_of_groups', [OrderedDict([('name', 'group1')])]),
+                ]),
+            ]
+        }
+        result = schema.execute(query)
+        assert not result.errors
+        assert result.data == expected
