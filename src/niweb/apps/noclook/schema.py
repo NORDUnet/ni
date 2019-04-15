4ba63790f9f03314f97acfc4f757e628d1bc3c0a
Francisco Fuentes <ffuentes@emergya.com>
2019-04-15 11:29:18 +0200

    functional list resolver

--- src/niweb/apps/noclook/schema.py
+++ src/niweb/apps/noclook/schema.py
@@ -7,12 +7,12 @@ from graphene_django.types import DjangoObjectTypeOptions
 from .models import *
 
 def get_srifield_resolver(field_name, field_type, rel_name='', rel_method=None):
-    def srifield_resolver(self, info, **kwargs):
+    def resolve_node_string(self, info, **kwargs):
         return self.get_node().data.get(field_name)
 
-    def resolve_is_roles(self, info, **kwargs):
+    def resolve_relationship_list(self, info, **kwargs):
         neo4jnode = self.get_node()
-        relations = neo4jnode.getattr(rel_method)()
+        relations = getattr(neo4jnode, rel_method)()
         roles = relations.get(rel_name)
 
         # this may be the worst way to do it, but it's just for a PoC
@@ -27,20 +27,25 @@ def get_srifield_resolver(field_name, field_type, rel_name='', rel_method=None):
         return ret
 
     if isinstance(field_type, graphene.String) or isinstance(field_type, graphene.Int):
-        return srifield_resolver
+        return resolve_node_string
     elif isinstance(field_type, graphene.List):
-        return resolve_is_roles
+        return resolve_relationship_list
     else:
-        return srifield_resolver
+        raise Exception(field_type.__class__)
+        return resolve_node_string
 
 class NIObjectField():
-    def __init__(self, field_type=graphene.String, type_args=None,
-                    type_kwargs=None, rel_name=None, rel_method=None, **kwargs):
-        self.field_type     = field_type
-        self.type_args      = type_args
-        self.type_kwargs    = type_kwargs
-        self.rel_name       = rel_name
-        self.rel_method     = rel_method
+    def __init__(self, field_type=graphene.String, manual_resolver=False,
+                    type_args=None, type_kwargs=None, rel_name=None,
+                    rel_method=None, not_null_list=False, **kwargs):
+
+        self.field_type      = field_type
+        self.manual_resolver = manual_resolver
+        self.type_args       = type_args
+        self.type_kwargs     = type_kwargs
+        self.rel_name        = rel_name
+        self.rel_method      = rel_method
+        self.not_null_list   = not_null_list
 
 class NIObjectType(DjangoObjectType):
     @classmethod
@@ -62,31 +67,37 @@ class NIObjectType(DjangoObjectType):
             fields_names = fields_names + ' ' + '({} / {})'.format(name, field.__dict__)
             field_fields = field.__dict__
 
-            field_type = field_fields.get('field_type')
-            type_kwargs = field_fields.get('type_kwargs')
-            type_args = field_fields.get('type_args')
-            rel_name = field_fields.get('rel_name')
-            rel_method = field_fields.get('rel_method')
+            field_type      = field_fields.get('field_type')
+            manual_resolver = field_fields.get('manual_resolver')
+            type_kwargs     = field_fields.get('type_kwargs')
+            type_args       = field_fields.get('type_args')
+            rel_name        = field_fields.get('rel_name')
+            rel_method      = field_fields.get('rel_method')
+            not_null_list   = field_fields.get('not_null_list')
 
             # adding the field
+            field_value = None
             if type_kwargs:
                 field_value = field_type(**type_kwargs)
             elif type_args:
                 field_value = field_type(*type_args)
+                if not_null_list:
+                    field_value = graphene.NonNull(field_type(*type_args))
             else:
                 field_value = field_type(**{})
 
             setattr(cls, name, field_value)
 
             # adding the resolver
-            setattr(cls, 'resolve_{}'.format(name), \
-                    get_srifield_resolver(
-                        name,
-                        field_type,
-                        rel_name,
-                        rel_method,
-                    )
-            )
+            if not manual_resolver:
+                setattr(cls, 'resolve_{}'.format(name), \
+                        get_srifield_resolver(
+                            name,
+                            field_value,
+                            rel_name,
+                            rel_method,
+                        )
+                )
 
         super(NIObjectType, cls).__init_subclass_with_meta__(
             model=NIObjectType._meta.model,
