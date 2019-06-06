f7bc78808c99cebd21beecf641be8e1669927381
Francisco Fuentes <ffuentes@emergya.com>
2019-06-06 11:33:15 +0200

    Role name isn't set on the contact props & no duplicates in contact list

--- src/niweb/apps/noclook/helpers.py
+++ src/niweb/apps/noclook/helpers.py
@@ -117,7 +117,7 @@ def form_update_node(user, handle_id, form, property_keys=None):
     meta_fields = ['relationship_location', 'relationship_end_a', 'relationship_end_b', 'relationship_parent',
                    'relationship_provider', 'relationship_end_user', 'relationship_customer', 'relationship_depends_on',
                    'relationship_user', 'relationship_owner', 'relationship_located_in', 'relationship_ports',
-                   'services_checked', 'relationship_responsible_for', 'relationship_connected_to']
+                   'services_checked', 'relationship_responsible_for', 'relationship_connected_to', 'role_name']
     nh, node = get_nh_node(handle_id)
     if not property_keys:
         property_keys = []
