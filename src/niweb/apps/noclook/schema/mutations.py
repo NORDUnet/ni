4ffd745d327d2d92c737561cabc7885ae0804ef7
Francisco Fuentes <ffuentes@emergya.com>
2019-07-04 14:31:29 +0200

    Added specific Organization mutation

--- src/niweb/apps/noclook/schema/mutations.py
+++ src/niweb/apps/noclook/schema/mutations.py
@@ -6,6 +6,7 @@ import norduniclient as nc
 
 from apps.noclook import activitylog, helpers
 from apps.noclook.forms import *
+from apps.noclook.models import Dropdown as DropdownModel
 
 from .core import NIMutationFactory, CreateNIMutation
 from .types import *
@@ -53,6 +54,10 @@ class NIContactMutationFactory(NIMutationFactory):
     class Meta:
         abstract = False
 
+def process_abuse_contact(request, form, nodehandler, relation_name):
+    group_nh = NodeHandle.objects.get(pk=form.cleaned_data[relation_name])
+    helpers.set_member_of(request.user, nodehandler, group_nh.handle_id)
+
 class NIOrganizationMutationFactory(NIMutationFactory):
     class NIMetaClass:
         create_form    = NewOrganizationForm
@@ -60,13 +65,63 @@ class NIOrganizationMutationFactory(NIMutationFactory):
         request_path   = '/'
         graphql_type   = Organization
         # create_include or create_exclude
-        # update_include or update_exclude
-        update_exclude = ('abuse_contact', 'primary_contact',
-                            'secondary_contact', 'it_technical_contact',
-                            'it_security_contact', 'it_manager_contact')
 
     class Meta:
         abstract = False
+
+class UpdateNIOrganizationMutation(UpdateNIMutation):
+    @classmethod
+    def do_request(cls, request, **kwargs):
+        form_class     = kwargs.get('form_class')
+        nimetaclass    = getattr(cls, 'NIMetaClass')
+        graphql_type   = getattr(nimetaclass, 'graphql_type')
+        nimetatype     = getattr(graphql_type, 'NIMetaType')
+        node_type      = getattr(nimetatype, 'ni_type').lower()
+        node_meta_type = getattr(nimetatype, 'ni_metatype').capitalize()
+        handle_id      = request.POST.get('handle_id')
+
+        # Get needed data from node
+        nh, organization = helpers.get_nh_node(handle_id)
+        relations = organization.get_relations()
+        out_relations = organization.get_outgoing_relations()
+        if request.POST:
+            form = form_class(request.POST.copy())
+            if form.is_valid():
+                # Generic node update
+                # use property keys to avoid inserting contacts as a string property of the node
+                property_keys = [
+                    'name', 'description', 'phone', 'website', 'customer_id', 'type', 'additional_info',
+                ]
+                helpers.form_update_node(request.user, organization.handle_id, form, property_keys)
+                # Set contacts
+                contact_fields = DropdownModel.get('organization_contact_types').as_choices(empty=False)
+                for field in contact_fields:
+                    if field[0] in form.cleaned_data:
+                        contact_data = form.cleaned_data[field[0]]
+                        if contact_data:
+                            if isinstance(contact_data, six.string_types):
+                                if contact_data:
+                                    helpers.create_contact_role_for_organization(request.user, organization, contact_data, field[1])
+                            else:
+                                helpers.link_contact_role_for_organization(request.user, organization, contact_data, field[1])
+
+                # Set child organizations
+                if form.cleaned_data['relationship_parent_of']:
+                    organization_nh = NodeHandle.objects.get(pk=form.cleaned_data['relationship_parent_of'])
+                    helpers.set_parent_of(request.user, organization, organization_nh.handle_id)
+                if form.cleaned_data['relationship_uses_a']:
+                    procedure_nh = NodeHandle.objects.get(pk=form.cleaned_data['relationship_uses_a'])
+                    helpers.set_uses_a(request.user, organization, procedure_nh.handle_id)
+
+                return { graphql_type.__name__.lower(): nh }
+        else:
+            # get the errors and return them
+            raise GraphQLError('Form errors: {}'.format(form.errors))
+
+    class NIMetaClass:
+        django_form = EditOrganizationForm
+        request_path   = '/'
+        graphql_type   = Organization
 
 class DeleteRelationship(relay.ClientIDMutation):
     class Input:
@@ -104,7 +159,7 @@ class NOCRootMutation(graphene.ObjectType):
     delete_contact      = NIContactMutationFactory.get_delete_mutation().Field()
 
     create_organization = NIOrganizationMutationFactory.get_create_mutation().Field()
-    update_organization = NIOrganizationMutationFactory.get_update_mutation().Field()
+    update_organization = UpdateNIOrganizationMutation.Field()
     delete_organization = NIOrganizationMutationFactory.get_delete_mutation().Field()
 
     delete_relationship = DeleteRelationship.Field()
