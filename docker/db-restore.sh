a137cf8bdc5e02d04ac7f3b2712125a4020c073c
Markus Krogh <markus@nordu.net>
2019-09-09 14:24:34 +0200

    Fix last-modified

--- docker/db-restore.sh
+++ docker/db-restore.sh
@@ -142,6 +142,16 @@ SELECT setval(pg_get_serial_sequence('"noclook_nordunetuniqueid"','id'), coalesc
 COMMIT;
 EOM
 
+msg "Reset last modified"
+cat <<EOM | docker exec -i $postgres_id psql -q -o /dev/null norduni ni
+BEGIN;
+UPDATE noclook_nodehandle
+SET modified=upd.timestamp
+FROM (SELECT MAX(timestamp) as timestamp, action_object_object_id FROM actstream_action GROUP BY action_object_object_id) as upd
+WHERE upd.action_object_object_id=noclook_nodehandle.handle_id::varchar;
+COMMIT;
+EOM
+
 echo ""
 echo "> Finished db-restore in $(duration $SECONDS )"
 echo "" 
