395a54e4803b95b754cb276559a92d6bb1f9bc07
Markus Krogh <markus@nordu.net>
2019-09-25 16:44:28 +0200

    Fix int to string comparison for key sorts

--- src/niweb/apps/noclook/templatetags/noclook_tags.py
+++ src/niweb/apps/noclook/templatetags/noclook_tags.py
@@ -358,8 +358,8 @@ def _equipment_spacer(units):
 
 
 def _rack_sort(item):
-    pos = item.get('node').data.get('rack_position', -1)
-    size = item.get('node').data.get('rack_units', 0) * -1
+    pos = int(item.get('node').data.get('rack_position', -1))
+    size = int(item.get('node').data.get('rack_units', 0)) * -1
 
     return (pos, size)
 
