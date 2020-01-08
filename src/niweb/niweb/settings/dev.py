1d8115be83a3e4816e2bd75eeef2b1288d2a33d7
Markus Krogh <markus@nordu.net>
2020-01-08 16:26:25 +0100

    Drop nose tests for now

--- src/niweb/niweb/settings/dev.py
+++ src/niweb/niweb/settings/dev.py
@@ -72,7 +72,7 @@ SECRET_KEY = environ.get('SECRET_KEY', 'development')
 ########## END SECRET CONFIGURATION
 
 ########## TESTING
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 ########## END TESTING
 
 GOOGLE_MAPS_API_KEY = environ.get('GOOGLE_MAPS_API_KEY', 'no-apikey')
