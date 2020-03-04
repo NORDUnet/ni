86fd40944a84a6df990b52faf03aa3ea98bc9769
Francisco Fuentes <ffuentes@emergya.com>
2020-03-04 09:51:02 +0100

    Add uses resolver, and Node inheritance

--- src/niweb/apps/noclook/schema/metatypes.py
+++ src/niweb/apps/noclook/schema/metatypes.py
@@ -5,20 +5,21 @@ from apps.noclook.models import NodeHandle
 import graphene
 
 ## metatype interfaces
-class Logical(graphene.Interface):
+class Logical(graphene.Node):
     name = graphene.String(required= True)
 
 
-class Relation(graphene.Interface):
+class Relation(graphene.Node):
     name = graphene.String(required= True)
     with_same_name = graphene.List(lambda:Relation)
+    uses = graphene.Field(Logical)
 
 
-class Physical(graphene.Interface):
+class Physical(graphene.Node):
     name = graphene.String(required= True)
 
 
-class Location(graphene.Interface):
+class Location(graphene.Node):
     name = graphene.String(required= True)
 
 
@@ -36,6 +37,22 @@ class RelationMixin:
             ids_samename = self.get_node().with_same_name().get('ids', None)
             if ids_samename and len(ids_samename) > 0:
                 ret = NodeHandle.objects.filter(handle_id__in=ids_samename)
+
+        return ret
+
+    def resolve_uses(self, info, **kwargs):
+        ret = None
+
+        if info.context and info.context.user.is_authenticated:
+            uses = self.get_node().get_uses()
+            if uses.get('Uses'):
+                uses_handle_id = uses['Uses'][0]['node'].handle_id
+
+                # check permission?
+                if uses_handle_id and \
+                    NodeHandle.objects.filter(handle_id=uses_handle_id):
+                    pass
+                    ret = NodeHandle.objects.get(handle_id=uses_handle_id)
 
         return ret
 
