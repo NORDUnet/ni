49f3fc91c8f6de76bf43e4cb73f9413691a3ee40
Francisco Fuentes <ffuentes@emergya.com>
2020-03-18 16:06:53 +0100

    Parametrization of the creation test for network's mutations

--- src/niweb/apps/noclook/tests/schema/network/test_simple_mutations.py
+++ src/niweb/apps/noclook/tests/schema/network/test_simple_mutations.py
@@ -10,65 +10,33 @@ from pprint import pformat
 from . import Neo4jGraphQLNetworkTest
 
 class GenericNetworkMutationTest(Neo4jGraphQLNetworkTest):
-        def delete(self, delete_mutation=None, id_str=None):
-            if not delete_mutation or not id_str:
-                raise Exception('Missconfigured test {}'.format(type(self)))
-
-            ## delete
-            query = """
-            mutation{{
-              {delete_mutation}(input:{{
-                id: "{id_str}"
-              }}){{
-                errors{{
-                  field
-                  messages
-                }}
-                success
-              }}
-            }}
-            """.format(delete_mutation=delete_mutation, id_str=id_str)
-
-            expected = OrderedDict([(delete_mutation,
-                        {
-                            'success': True,
-                            'errors': None
-                        }
-                    )])
-
-            result = schema.execute(query, context=self.context)
-            assert not result.errors, pformat(result.errors, indent=1)
-
-            assert result.data == expected, '{} \n != {}'.format(
-                                                    pformat(result.data, indent=1),
-                                                    pformat(expected, indent=1)
-                                                )
-
-            # check node delete
-            handle_id = relay.Node.from_global_id(id_str)[1]
-            exists = NodeHandle.objects.filter(handle_id=handle_id).exists()
-            self.assertFalse(exists)
-
-            return result.data[delete_mutation]['success']
-
-## Organizations
-class GenericOrganizationTest(GenericNetworkMutationTest):
-    def create(self, create_mutation=None, entityname=None):
-        if not create_mutation or not entityname:
+    def create_mutation(self, create_mutation=None, entityname=None, data=None):
+        if not create_mutation or not entityname or not data:
             raise Exception('Missconfigured test {}'.format(type(self)))
 
-        data_generator = FakeDataGenerator()
-        the_name = data_generator.rand_person_or_company_name()
-        the_url = data_generator.fake.url()
-        the_description = data_generator.fake.paragraph()
+        input_str = None
+        inputs = []
+        values = {}
+
+        for data_name, data_f in data.items():
+            data_f_val = data_f()
+
+            inputs.append('{data_name}: "{data_f}",'.format(
+                data_name=data_name,
+                data_f=data_f_val
+            ))
+            values[data_name] = data_f_val
+
+        values['id'] = None
+
+        input_str = "\n".join(inputs)
+        query_attr = "\n".join(data.keys())
 
         ## create
         query = """
         mutation{{
           {create_mutation}(input:{{
-            name: "{the_name}",
-            url: "{the_url}",
-            description: "{the_description}"
+            {input_str}
           }}){{
             errors{{
               field
@@ -76,33 +44,25 @@ class GenericOrganizationTest(GenericNetworkMutationTest):
             }}
             {entityname}{{
               id
-              name
-              url
-              description
+              {query_attr}
             }}
           }}
         }}
-        """.format(create_mutation=create_mutation, the_name=the_name,
-                    the_url=the_url, the_description=the_description,
-                    entityname=entityname)
-
-        expected = OrderedDict([(create_mutation,
-                    {
-                        entityname: {
-                            'id': None,
-                            'name': the_name,
-                            'url': the_url,
-                            'description': the_description,
-                        },
-                        'errors': None
-                    }
-                )])
+        """.format(create_mutation=create_mutation, input_str=input_str,
+                    query_attr=query_attr, entityname=entityname)
 
         result = schema.execute(query, context=self.context)
         assert not result.errors, pformat(result.errors, indent=1)
 
         id_str = result.data[create_mutation][entityname]['id']
-        expected[create_mutation][entityname]['id'] = id_str
+        values['id'] = id_str
+
+        expected = OrderedDict([(create_mutation,
+            {
+                entityname: values,
+                'errors': None
+            }
+        )])
 
         assert result.data == expected, '{} \n != {}'.format(
                                                 pformat(result.data, indent=1),
@@ -110,16 +70,70 @@ class GenericOrganizationTest(GenericNetworkMutationTest):
                                             )
 
         # check node creation
+        values.pop('id', None)
         handle_id = relay.Node.from_global_id(id_str)[1]
         nh = NodeHandle.objects.get(handle_id=handle_id)
-        test_data = {
-            'name': the_name,
-            'url': the_url,
-            'description': the_description,
-        }
-        self.assertDictContainsSubset(test_data, nh.get_node().data)
+        self.assertDictContainsSubset(values, nh.get_node().data)
 
         return id_str
+
+
+    def delete(self, delete_mutation=None, id_str=None):
+        if not delete_mutation or not id_str:
+            raise Exception('Missconfigured test {}'.format(type(self)))
+
+        ## delete
+        query = """
+        mutation{{
+          {delete_mutation}(input:{{
+            id: "{id_str}"
+          }}){{
+            errors{{
+              field
+              messages
+            }}
+            success
+          }}
+        }}
+        """.format(delete_mutation=delete_mutation, id_str=id_str)
+
+        expected = OrderedDict([(delete_mutation,
+                    {
+                        'success': True,
+                        'errors': None
+                    }
+                )])
+
+        result = schema.execute(query, context=self.context)
+        assert not result.errors, pformat(result.errors, indent=1)
+
+        assert result.data == expected, '{} \n != {}'.format(
+                                                pformat(result.data, indent=1),
+                                                pformat(expected, indent=1)
+                                            )
+
+        # check node delete
+        handle_id = relay.Node.from_global_id(id_str)[1]
+        exists = NodeHandle.objects.filter(handle_id=handle_id).exists()
+        self.assertFalse(exists)
+
+        return result.data[delete_mutation]['success']
+
+## Organizations
+class GenericOrganizationTest(GenericNetworkMutationTest):
+    def create_mutation(self, create_mutation=None, entityname=None):
+        data_generator = FakeDataGenerator()
+        data = {
+            'name': data_generator.rand_person_or_company_name,
+            'url': data_generator.fake.url,
+            'description': data_generator.fake.paragraph,
+        }
+
+        return super().create_mutation(
+            create_mutation=create_mutation,
+            entityname=entityname,
+            data=data
+        )
 
     def edit(self, update_mutation=None, entityname=None, id_str=None):
         if not update_mutation or not entityname or not id_str:
@@ -230,7 +244,7 @@ class GenericOrganizationTest(GenericNetworkMutationTest):
 
 class CustomerTest(GenericOrganizationTest):
     def test_crud(self):
-        id_str = self.create(
+        id_str = self.create_mutation(
             create_mutation='create_customer',
             entityname='customer'
         )
@@ -249,7 +263,7 @@ class CustomerTest(GenericOrganizationTest):
 
 class EndUserTest(GenericOrganizationTest):
     def test_crud(self):
-        id_str = self.create(
+        id_str = self.create_mutation(
             create_mutation='create_enduser',
             entityname='enduser'
         )
@@ -268,7 +282,7 @@ class EndUserTest(GenericOrganizationTest):
 
 class ProviderTest(GenericOrganizationTest):
     def test_crud(self):
-        id_str = self.create(
+        id_str = self.create_mutation(
             create_mutation='create_provider',
             entityname='provider'
         )
@@ -287,7 +301,7 @@ class ProviderTest(GenericOrganizationTest):
 
 class SiteOwnerTest(GenericOrganizationTest):
     def test_crud(self):
-        id_str = self.create(
+        id_str = self.create_mutation(
             create_mutation='create_siteowner',
             entityname='siteowner'
         )
