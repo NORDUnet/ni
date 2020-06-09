55a6e03a9b5a586afb4918ce719c97dc75d6eb2e
Francisco Fuentes <ffuentes@emergya.com>
2020-06-09 12:40:32 +0200

    Full test of activitylog

--- src/niweb/apps/noclook/tests/schema/test_activitylog.py
+++ src/niweb/apps/noclook/tests/schema/test_activitylog.py
@@ -8,7 +8,7 @@ from graphene import relay
 from niweb.schema import schema
 from pprint import pformat
 
-class Neo4jGraphQLActivitylogTest(Neo4jGraphQLGenericTest):
+class ActivitylogTest(Neo4jGraphQLGenericTest):
     def query_activity(self, contextname):
         query = '''
         {{
@@ -45,6 +45,8 @@ class Neo4jGraphQLActivitylogTest(Neo4jGraphQLGenericTest):
 
         return result
 
+
+class Neo4jGraphQLActivitylogTest(ActivitylogTest):
     def test_crud(self):
         ## check available contexts/modules
         query ='''
@@ -182,8 +184,12 @@ class Neo4jGraphQLActivitylogTest(Neo4jGraphQLGenericTest):
         # check activity
         result = self.query_activity('Community')
         assert not result.errors, pformat(result.errors, indent=1)
-        
+
+        ## check result length
         result_actions = result.data['getContextActivity']['edges']
+
+        self.assertTrue(len(result_actions) == 1)
+
         result_action = result_actions[0]['node']
 
         # check action verb
@@ -196,23 +202,101 @@ class Neo4jGraphQLActivitylogTest(Neo4jGraphQLGenericTest):
         self.assertEqual(result_action['action_object'], None)
 
 
-class Neo4jGraphQLActivitylogPermissionTest(Neo4jGraphQLGenericTest):
+class Neo4jGraphQLActivitylogPermissionTest(ActivitylogTest):
     def setUp(self, group_dict=None):
         group_dict = {
             'community': {
-                'admin': False
+                'admin': False,
+                'read': True,
+                'list': True,
+                'write': True,
             },
             'network': {
+                'admin': True,
                 'read': False,
-                'list': False
+                'list': False,
+                'write': True,
             }
         }
 
         super(Neo4jGraphQLActivitylogPermissionTest, self).setUp(group_dict=group_dict)
 
 
-    def test_crud(self):
-        pass
+    def test_permissions(self):
         ## permissions
-        # check a node that the user is not allowed to read
-        # check the actor object for a node without the right admin permission
+        ## check a node that the user is not allowed to read
+        # create a node (end user)
+        query = '''
+        mutation{
+          create_endUser(input:{
+            name: "Test",
+            description: "Lorem ipsum"
+            url: "https://sunet.se"
+          }){
+            errors{
+              field
+              messages
+            }
+            endUser{
+              id
+              name
+              description
+              url
+            }
+          }
+        }
+        '''
+
+        result = schema.execute(query, context=self.context)
+        assert not result.errors, pformat(result.errors, indent=1)
+
+        assert not result.data['create_endUser']['errors'], \
+            pformat(result.data['create_endUser']['errors'], indent=1)
+
+        # check activity
+        result = self.query_activity('Network')
+        assert not result.errors, pformat(result.errors, indent=1)
+
+        ## check result length
+        ## (should be zero as we don't have network list and read rights)
+        result_actions = result.data['getContextActivity']['edges']
+        self.assertTrue(len(result_actions) == 0)
+
+        ## check the actor object for a node without the right admin permission
+        # create a node (group)
+        query = '''
+        mutation{
+          create_group(input:{
+            name: "Test group"
+            description: "Lorem ipsum dolor sit amet"
+          }){
+            errors{
+              field
+              messages
+            }
+            group{
+              id
+              name
+              description
+            }
+          }
+        }
+        '''
+
+        result = schema.execute(query, context=self.context)
+        assert not result.errors, pformat(result.errors, indent=1)
+
+        assert not result.data['create_group']['errors'], \
+            pformat(result.data['create_group']['errors'], indent=1)
+
+        # check activity
+        result = self.query_activity('Community')
+        assert not result.errors, pformat(result.errors, indent=1)
+
+        ## check result length
+        ## (should be one as we have community list and read rights)
+        result_actions = result.data['getContextActivity']['edges']
+        self.assertTrue(len(result_actions) == 1)
+
+        result_action = result_actions[0]['node']
+        self.assertEqual(result_action['actor'], None)
