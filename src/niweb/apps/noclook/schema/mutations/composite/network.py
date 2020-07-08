8f3e12be659d3a39da643796896817ccb037cf64
Francisco Fuentes <ffuentes@emergya.com>
2020-07-08 12:17:59 +0200

    Removed unnecesary secondary mutation factory for external equipment

--- src/niweb/apps/noclook/schema/mutations/composite/network.py
+++ src/niweb/apps/noclook/schema/mutations/composite/network.py
@@ -109,13 +109,7 @@ class CompositeExternalEquipmentMutation(CompositeMutation):
     class Input:
         pass
 
-    @classmethod
-    def link_slave_to_master(cls, user, master_nh, slave_nh):
-        helpers.set_has(user, master_nh.get_node(), slave_nh.handle_id)
-
     class NIMetaClass:
         graphql_type = ExternalEquipment
-        graphql_subtype = Port
         main_mutation_f = NIExternalEquipmentMutationFactory
-        secondary_mutation_f = NIPortMutationFactory
         context = sriutils.get_network_context()
