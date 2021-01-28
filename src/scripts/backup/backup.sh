ef99f63c40ef2f720694e1d56b9b95548fec5fd2
Markus Krogh <markus@nordu.net>
2021-01-28 13:28:25 +0100

    backup script should show print curl errors

--- src/scripts/backup/backup.sh
+++ src/scripts/backup/backup.sh
@@ -36,15 +36,15 @@ rm -r json
 
 if [ -z "$DISABLE_UPLOAD" ]; then
   # Push data
-  curl -s -u "$BACKUPUSER" -T "$BACKUPDIR/postgres-$TODAY.sql.gz" "$BACKUPURL/"
-  curl -s -u "$BACKUPUSER" -T "$BACKUPDIR/ni_data-$TODAY.tar.gz" "$BACKUPURL/"
+  curl -sS -u "$BACKUPUSER" -T "$BACKUPDIR/postgres-$TODAY.sql.gz" "$BACKUPURL/"
+  curl -sS -u "$BACKUPUSER" -T "$BACKUPDIR/ni_data-$TODAY.tar.gz" "$BACKUPURL/"
   # Make latest
   set +e
-  curl -s -X DELETE -u "$BACKUPUSER" "$BACKUPURL/ni_data.tar.gz"
-  curl -s -X DELETE -u "$BACKUPUSER" "$BACKUPURL/postgres.sql.gz"
+  curl -sS -X DELETE -u "$BACKUPUSER" "$BACKUPURL/ni_data.tar.gz"
+  curl -sS -X DELETE -u "$BACKUPUSER" "$BACKUPURL/postgres.sql.gz"
   set -e
-  curl -s -X COPY -u "$BACKUPUSER" -H "Destination: $BACKUPURL/postgres.sql.gz" "$BACKUPURL/postgres-$TODAY.sql.gz"
-  curl -s -X COPY -u "$BACKUPUSER" -H "Destination: $BACKUPURL/ni_data.tar.gz" "$BACKUPURL/ni_data-$TODAY.tar.gz"
+  curl -sS -X COPY -u "$BACKUPUSER" -H "Destination: $BACKUPURL/postgres.sql.gz" "$BACKUPURL/postgres-$TODAY.sql.gz"
+  curl -sS -X COPY -u "$BACKUPUSER" -H "Destination: $BACKUPURL/ni_data.tar.gz" "$BACKUPURL/ni_data-$TODAY.tar.gz"
 fi
 
 # Cleanup
