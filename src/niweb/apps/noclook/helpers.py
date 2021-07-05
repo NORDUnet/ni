931ec847223d7ca45b8ddc5ca33f11209930e9a3
Markus Krogh <markus@nordu.net>
2021-07-05 08:55:51 +0200

    Fix uniquness check to include the name of the node

--- src/niweb/apps/noclook/helpers.py
+++ src/niweb/apps/noclook/helpers.py
@@ -222,10 +222,10 @@ def get_or_create_site_unique_node_handle(user, node_name, slug, node_meta_type,
     node_type = slug_to_node_type(slug, create=True)
     # XXX: Not at all transactional
     q = """
-    MATCH (n:Node)<-[:Has]-(s:Site {handle_id: {handle_id}})
+    MATCH (n:Node {name: {node_name}})<-[:Has]-(s:Site {handle_id: {handle_id}})
     RETURN n
     """
-    result = nc.query_to_list(nc.graphdb.manager, q, handle_id=site.handle_id)
+    result = nc.query_to_list(nc.graphdb.manager, q, handle_id=site.handle_id, node_name=node_name)
     if len(result) > 1:
         raise Exception('Multiple {} named {} exists ({})'.format(node_type, node_name, [n['n'].get('handle_id') for n in result]))
     if len(result) == 1:
